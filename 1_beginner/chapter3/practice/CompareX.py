ba352a15ca8269dbf05f157fee32a03a8db6c27b
Miserabl <42326227+Miserabl@users.noreply.github.com>
2020-06-11 16:41:18 -0700

    Create CompareX.py

---
+++ 1_beginner/chapter3/practice/CompareX.py
@@ -0,0 +1,8 @@
+# You start with x = 10. Do something to X so when you run the program, it returns False. 
+# Do not mess with the end or start of the program, just write one line of code in between
+
+x = 10
+
+
+
+x < 15
