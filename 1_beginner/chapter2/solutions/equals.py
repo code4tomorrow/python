4a783826e88a3898bd7085f070f585c02f312291
Miserabl <42326227+Miserabl@users.noreply.github.com>
2020-06-11 13:19:24 -0700

    Create equals.py

---
+++ 1_beginner/chapter2/solutions/equals.py
@@ -0,0 +1,7 @@
+#Write a program that takes an integer from the user and turns it into a floating point number. 
+#Then, print ‘ (floating point number)  =  (the integer from the user)
+
+
+x = input()
+y = float(x)
+print(y, '=', x)
