fb32c84e93f761429351fa09220921a172efd6b6
Miserabl <42326227+Miserabl@users.noreply.github.com>
2020-06-11 16:03:46 -0700

    Create C-Volume.py

---
+++ 1_beginner/chapter3/practice/C-Volume.py
@@ -0,0 +1,4 @@
+# Design a program that asks the user for the height and radius of a cylinder and returns a volume
+# Make sure you have a way to differenicate between the values when asking for them 
+
+
