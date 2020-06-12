a8869739da5c35def28238b9368d70d466630ac4
Miserabl <42326227+Miserabl@users.noreply.github.com>
2020-06-12 15:14:32 -0700

    Create TV.py

---
+++ 1_beginner/chapter5/practice/TV.py
@@ -0,0 +1,3 @@
+# Pretend you just got a 50 on your test, and you mom says you can’t watch TV until you get above an 80. 
+#(HINT: comparison operators). You increase your test score 10 points per day (iterations).
+
