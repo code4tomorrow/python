eeea6274ff7cdadc488404fd4747545bbe645e54
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:55:09 -0400

    Create power.py

---
+++ 1_beginner/chapter3/practice/power.py
@@ -0,0 +1,6 @@
+# Power
+# Write a program that accepts two numbers, x and y.
+# Compute and print the quantity (x+y)^5. Display the entire equation.
+# The program should accept decimals as inputs.
+
+# Write code here
