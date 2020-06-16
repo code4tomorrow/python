4ef15c6d1ae6cdc5a57d8a2ba1cd9c7b9bca5385
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:58:24 -0400

    Create temperature.py

---
+++ 1_beginner/chapter3/solutions/temperature.py
@@ -0,0 +1,12 @@
+# Temperature
+# Write a program that converts Celsius to Fahrenheit.
+# The formula is: F = C * 9/5 + 32
+
+# Get the celsius value as input
+celsius = float(input("Enter a temperature in degrees Celsius: "))
+
+# Convert to fahrenheit
+fahrenheit = celsius * 9/5 + 32
+
+# Display answer
+print("The temperature in degrees Fahrenheit is " + str(fahrenheit))
