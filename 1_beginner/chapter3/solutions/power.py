22002084fc98eb04a59c3ef9633cbe454d51c863
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:56:03 -0400

    Create power.py

---
+++ 1_beginner/chapter3/solutions/power.py
@@ -0,0 +1,14 @@
+# Power
+# Write a program that accepts two numbers, x and y.
+# Compute and print the quantity (x+y)^5. Display the entire equation.
+# The program should accept decimals as inputs.
+
+# Get the float inputs
+x = float(input("Enter x: "))
+y = float(input("Enter y: "))
+
+# Calculate the answer
+ans = (x+y)**5
+
+# Display the equation and cast the numbers to strings
+print("(" + str(x) + " + " + str(y) + ")^5 = " + str(ans))
