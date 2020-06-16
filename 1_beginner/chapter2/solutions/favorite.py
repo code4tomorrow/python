5a60fab92ee4e4955a3738ceae8c9105db200e3f
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:40:34 -0400

    Create favorite.py

---
+++ 1_beginner/chapter2/solutions/favorite.py
@@ -0,0 +1,11 @@
+# Favorite
+# Write a program that asks the user for their favorite food, favorite drink, and the name of their favorite person. 
+# Output a statement that uses all of this information. (Be creative!)
+
+# Get user inputs and store as variables
+favorite_food = input("What is your favorite food? ")
+favorite_drink = input("What is your favorite drink? ")
+favorite_person = input("Who is your favorite person? ")
+
+# Display output
+print(favorite_person + " bought you " + favorite_food + " and " + favorite_drink + ".")
