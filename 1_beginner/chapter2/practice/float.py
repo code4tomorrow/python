3d4c8a38b7909d2390d5e25e2b137bef78268d4a
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:47:33 -0400

    Create float.py

---
+++ 1_beginner/chapter2/practice/float.py
@@ -0,0 +1,8 @@
+# Float
+# Write a program that takes an integer from the user and turns it into a floating point number. 
+# Store this floating point number in a variable. 
+# Then print: (floating point number)  =  (the integer from the user). 
+# Also print the type of the floating point variable.
+# Remember! type(variable_name) will return the data type of a variable
+
+# Write code here
