1e4d5774c4c37319bcdbabcb2d67002a13902eb3
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:39:52 -0400

    Create favorite.py

---
+++ 1_beginner/chapter2/practice/favorite.py
@@ -0,0 +1,5 @@
+# Favorite
+# Write a program that asks the user for their favorite food, favorite drink, and the name of their favorite person. 
+# Output a statement that uses all of this information. (Be creative!)
+
+# Write code here
