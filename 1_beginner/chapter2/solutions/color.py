6905f724309ebde8790c7baf76a1d02758cd3d86
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-16 13:36:07 -0400

    Create color.py

---
+++ 1_beginner/chapter2/solutions/color.py
@@ -0,0 +1,8 @@
+# Color
+# Write a program that asks the user for their favorite color, and outputs the message, “I love [color], too!”
+
+# Ask the user for their favorite color and store this in a variable
+color = input("Enter your favorite color: ")
+
+# Display the output
+print("I love " + color + ", too!")
