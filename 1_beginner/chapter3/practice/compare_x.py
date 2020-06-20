ec382b14d7c2771ef572be1ee8eb7301911a0fc1
Rebecca Dang <ph.rdang@gmail.com>
2020-06-20 15:08:02 -0700

    Renamed Compare X and clarify instructions

---
+++ 1_beginner/chapter3/practice/compare_x.py
@@ -0,0 +1,8 @@
+# You start with x = 10. 
+# Compare x with another number using
+# a comparison operator such that if you
+# print x (comparison operator) (another number),
+# it prints False.
+
+x = 10
+# write code here
--- 1_beginner/chapter3/solutions/CompareX.py
+++
@@ -1,8 +0,0 @@
-# You start with x = 10. Do something to X so when you run the program, it returns False. 
-# Do not mess with the end or start of the program, just write one line of code in between
-
-x = 10
-
-x = X + 6
-
-x < 15
