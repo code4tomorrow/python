46c9d77573a4a915351310b895e10dd3ca8abc51
Rebecca Dang <ph.rdang@gmail.com>
2020-06-20 13:11:33 -0700

    Add Print Data Types exercise

---
+++ 1_beginner/chapter2/practice/print_data_types.py
@@ -0,0 +1,3 @@
+# Print Data Types
+# Come up with 3 examples each of 
+# floating numbers, integers, and strings and print them.
---
+++ 1_beginner/chapter2/solutions/print_data_types.py
@@ -0,0 +1,18 @@
+# Print Data Types
+# Come up with 3 examples each of 
+# floating numbers, integers, and strings and print them.
+
+# floats
+print(1.56)
+print(32.0)
+print(-35.25)
+
+# integers
+print(25)
+print(0)
+print(-1)
+
+# strings
+print("Tahiti, it's a magical place")
+print('May the Force be with you')
+print("Hey guys")
