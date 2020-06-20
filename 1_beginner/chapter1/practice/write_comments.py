aec4ffc61ff51e5ce8fb366e8655ff0ad66d7001
Rebecca Dang <ph.rdang@gmail.com>
2020-06-20 13:04:17 -0700

    Add Write Comments exercise

---
+++ 1_beginner/chapter1/practice/write_comments.py
@@ -0,0 +1,3 @@
+# Write Comments
+# Write 3 comments about anything you want, 
+# like your daily routine or your favorite music artist.
---
+++ 1_beginner/chapter1/solutions/write_comments.py
@@ -0,0 +1,7 @@
+# Write Comments
+# Write 3 comments about anything you want, 
+# like your daily routine or your favorite music artist.
+
+# I wake up at 8 AM and eat breakfast
+# My favorite artist is Sara Bareilles
+# Python is cool!
