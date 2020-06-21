cb50088a5efed63df77fa4caea972f6c664e0369
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-21 14:20:31 -0400

    Update float.py

--- 1_beginner/chapter2/solutions/float.py
+++ 1_beginner/chapter2/solutions/float.py
@@ -1,6 +1,6 @@
 # Float
-# Write a program that takes an integer from the user and turns it into a floating point number. 
-# Store this floating point number in a variable. 
+# Write a program that takes a number from the user and stores it in a variable.
+# Cast the number to an integer and store in another variable. 
 # Then print: (floating point number)  =  (the integer from the user). 
 # Also print the type of the floating point variable.
 
