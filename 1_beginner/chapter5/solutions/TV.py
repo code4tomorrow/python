47f47e90b196a0dc83c2b9b324a8753f2b43fba4
Miserabl <42326227+Miserabl@users.noreply.github.com>
2020-06-21 02:27:55 -0700

    Update TV.py

--- 1_beginner/chapter5/solutions/TV.py
+++ 1_beginner/chapter5/solutions/TV.py
@@ -1,11 +1,11 @@
-# Pretend you just got a 50 on your test,
-# and you mom says you can’t watch TV until you get above an 80.
+# Pretend you just got a 37 on your test,
+# and you mom says you can’t watch TV until you get above an 84.
 # (HINT: comparison operators). You increase your test score
-# 10 points per day (iterations).
+# 6 points per day (iterations).
 
-x = 50
+x = 37
 d = 0
-while x < 80:
-    x += 10
+while x < 84:
+    x += 6
     d += 1
 print(d)
