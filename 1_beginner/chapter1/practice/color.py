00ce110f2ef06353debcae13b5399ae0e4d107e1
Rebecca Dang <ph.rdang@gmail.com>
2020-06-20 19:51:52 -0700

    Fix Color exercise line length, add gitignore

--- 1_beginner/chapter1/practice/color.py
+++ 1_beginner/chapter1/practice/color.py
@@ -1,4 +1,6 @@
 # Color
-# Write a program that asks the user for their favorite color, and outputs the message, “I love [color], too!”
+# Write a program that asks the user
+# for their favorite color, and outputs
+# the message, “I love [color], too!”
 
 # Write your code here
--- 1_beginner/chapter1/solutions/color.py
+++ 1_beginner/chapter1/solutions/color.py
@@ -1,5 +1,7 @@
 # Color
-# Write a program that asks the user for their favorite color, and outputs the message, “I love [color], too!”
+# Write a program that asks the user
+# for their favorite color, and outputs
+# the message, “I love [color], too!”
 
 # Ask the user for their favorite color and store this in a variable
 color = input("Enter your favorite color: ")
