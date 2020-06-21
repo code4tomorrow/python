b0725d00b1887d94158b2fef92e0129422652afd
Miserabl <42326227+Miserabl@users.noreply.github.com>
2020-06-21 02:28:00 -0700

    Update TV.py

--- 1_beginner/chapter5/practice/TV.py
+++ 1_beginner/chapter5/practice/TV.py
@@ -1,6 +1,6 @@
-# Pretend you just got a 50 on your test, and you mom says you can’t watch TV until you get above an 80. 
-#(HINT: comparison operators). You increase your test score 10 points per day (iterations).
-
+# Pretend you just got a 37 on your test, and you mom says you can’t watch TV until you get above an 84. 
+#(HINT: comparison operators). You increase your test score 6 points per day (iterations).
+#Write a program that tells you after how many days you'll be able to watch TV
 
 
 
