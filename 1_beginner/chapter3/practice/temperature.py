6b0f57cd4169f46f93dd6ca6bad7d65e530e175c
Rebecca Dang <ph.rdang@gmail.com>
2020-06-23 12:42:56 -0700

    Processed Neha P.'s Ch. 3 problems

--- 1_beginner/chapter3/practice/temperature.py
+++ 1_beginner/chapter3/practice/temperature.py
@@ -1,5 +1,7 @@
 # Temperature
 # Write a program that converts Celsius to Fahrenheit.
+# It should ask the user for the temperature in
+# degrees Celsius and then print the temperature in degrees Fahrenheit.
 # The formula is: F = C * 9/5 + 32
 
 # Write code here
