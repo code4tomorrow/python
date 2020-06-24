5f0b6ea828b9534d47c56d8c1244cc5a7208424c
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-24 18:06:42 -0400

    Create lunch_tables.py

---
+++ 1_beginner/chapter3/solutions/lunch_tables.py
@@ -0,0 +1,13 @@
+# Lunch Tables
+# Ask the user to input how many people are in the lunchroom, and how many people can sit at each table.
+# Output how many people will be left without a table (the number of outcasts).
+
+# Get user input
+people = input("How many people are in the lunchroom? ")
+table_limit = input("How many people can sit at a table? ")
+
+# Calculate the number of outcasts (the remainder)
+outcasts = int(people) % int(table_limit)
+
+# Display output
+print("Outcasts:", outcasts)
