ba4f480ac3b1a667d0d4ca31fb50858e7ffea61e
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-24 22:33:58 -0400

    Update lunch_tables.py

--- 1_beginner/chapter3/practice/lunch_tables.py
+++ 1_beginner/chapter3/practice/lunch_tables.py
@@ -1,5 +1,8 @@
-# Lunch Tables
-# Ask the user to input how many people are in the lunchroom, and how many people can sit at each table.
-# Output how many people will be left without a table.
+"""
+Lunch Tables
+Ask the user to input how many people are in the lunchroom,
+and how many people can sit at each table.
+Output how many people will be left without a table.
+"""
 
 # Write code here
