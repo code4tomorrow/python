7d70707bda888d375ecc00fa15182d17f247a7a0
Rebecca Dang <ph.rdang@gmail.com>
2020-06-25 12:58:00 -0700

    Modify instructions, fix style for Grade

--- 1_beginner/chapter4/solutions/grade.py
+++ 1_beginner/chapter4/solutions/grade.py
@@ -1,7 +1,17 @@
-# Grade
-# Write a program that asks the user to enter
-# the score for a student's test.
-# Print the letter grade that the test score receives.
+'''
+Grade
+Write a program that asks the user to enter
+the score for a student's test.
+The letter grades are as follows:
+
+A: >= 90
+B: >= 80
+c: >= 70
+D: >= 60
+F: < 60
+
+Print the letter grade that the test score receives.
+'''
 
 score = float(input("Enter test score: "))
 if score >= 90:
@@ -15,10 +25,11 @@ elif score >= 60:
 else:
     print("F")
 
-#See if you can write the same program, but without using >= anywhere
-#This question is pretty tricky, so don't worry if you can't figure it out. We will go over it in class
+'''
+See if you can write the same program,
+but without using >= anywhere.
+'''
 
-#write code here
 score = float(input("Enter test score: "))
 if score < 60:
     print("F")
