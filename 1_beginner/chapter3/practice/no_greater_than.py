cd8d0072474ce8549dc7d03ef821a19f122a2a78
Rebecca Dang <ph.rdang@gmail.com>
2020-06-25 11:22:38 -0700

    Modify instructions No Greater Than for clarity and style

--- 1_beginner/chapter3/practice/no_greater_than.py
+++ 1_beginner/chapter3/practice/no_greater_than.py
@@ -1,7 +1,12 @@
 '''
-Create a program that takes a POSITIVE integer as an input and checks if it is greater than 100.
-Print true if it is, and False if it isn't
+Create a program that takes a POSITIVE integer
+as an input and checks if it is greater than 100.
+Print True if it is, and False if it isn't
 
-YOU MAY NOT USE THE GREATER THAN or LESS THAN OPERATOR ( >, or >=). Find a way to do this problem only using only the == operator and any math operators you want.
-
+YOU MAY NOT USE THE GREATER THAN or
+LESS THAN OPERATOR (>, <, >=, or ,=).
+Find a way to do this problem only
+using only the == operator and any math operators you want.
 '''
+
+# write code here
