985c2eee5f549ff1280367bb878ac9bc242d328b
Rebecca Dang <ph.rdang@gmail.com>
2020-06-24 21:35:02 -0700

    Clarify instructions for Cookies, fix style

--- 1_beginner/chapter3/practice/cookies.py
+++ 1_beginner/chapter3/practice/cookies.py
@@ -1,13 +1,14 @@
 """
 Cookies
 Prompt the user with the following questions:
-  How many cookies did they make?
-  How many cookies did their friend make?
+  How many cookies did you make?
+  How many cookies did your friend make?
   How many cookies are burnt?
-  How many friends do they have?
+  How many friends do you have?
 
-Output the number of cookies that will be left over if you
-throw out all the burnt cookies, and all the friends get the same amount.
+Print the total number of unburnt cookies.
+Then output the number of cookies that will be left over if you
+throw out all the burnt cookies and all the friends get the same amount.
 """
 
 # Write code here
--- 1_beginner/chapter3/solutions/cookies.py
+++ 1_beginner/chapter3/solutions/cookies.py
@@ -1,14 +1,14 @@
 """
 Cookies
 Prompt the user with the following questions:
-  How many cookies did they make?
-  How many cookies did their friend make?
+  How many cookies did you make?
+  How many cookies did your friend make?
   How many cookies are burnt?
-  How many friends do they have?
+  How many friends do you have?
 
-Output the number of cookies that will be left over
-if you throw out all the burnt cookies,
-and all the friends get the same amount.
+Print the total number of unburnt cookies.
+Then output the number of cookies that will be left over if you
+throw out all the burnt cookies and all the friends get the same amount.
 """
 
 # Get all the inputs and store as integers in variables
@@ -16,7 +16,7 @@ your_cookies = int(input("How many cookies did you make? "))
 friend_cookies = int(input("How many cookies did your friend make? "))
 burnt_cookies = int(input("How many of those cookies are burnt? "))
 friend_count = int(input("How many friends do you have? "))
-print() #  This makes a new empty line (a line break)
+print()  # This makes a new empty line (a line break)
 
 # Calculate the total number of unburnt cookies
 total = your_cookies + friend_cookies - burnt_cookies
@@ -25,7 +25,7 @@ total = your_cookies + friend_cookies - burnt_cookies
 # Cast the integer variable "total" to a string
 print("Number of unburnt cookies: " + str(total))
 
-# Calculate the REMAINDER of cookies 
+# Calculate the REMAINDER of cookies
 # after dividing the total between your friends
 remainder = total % friend_count
 
