66062368d8b7124e775d401a3819175e81ca4ecc
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-24 22:56:26 -0400

    Update cookies.py

--- 1_beginner/chapter3/solutions/cookies.py
+++ 1_beginner/chapter3/solutions/cookies.py
@@ -6,8 +6,8 @@ Prompt the user with the following questions:
   How many cookies are burnt?
   How many friends do they have?
 
-Output the number of cookies that will be left over 
-if you throw out all the burnt cookies, 
+Output the number of cookies that will be left over
+if you throw out all the burnt cookies,
 and all the friends get the same amount.
 """
 
@@ -16,16 +16,17 @@ your_cookies = int(input("How many cookies did you make? "))
 friend_cookies = int(input("How many cookies did your friend make? "))
 burnt_cookies = int(input("How many of those cookies are burnt? "))
 friend_count = int(input("How many friends do you have? "))
-print() # This makes a new empty line (a line break)
+print() #  This makes a new empty line (a line break)
 
 # Calculate the total number of unburnt cookies
 total = your_cookies + friend_cookies - burnt_cookies
 
 # Display the number of unburnt cookies.
-# Cast the integer variable "total" to a string before attempting string concatenation.
+# Cast the integer variable "total" to a string
 print("Number of unburnt cookies: " + str(total))
 
-# Calculate the REMAINDER of cookies after dividing the total between your friends
+# Calculate the REMAINDER of cookies 
+# after dividing the total between your friends
 remainder = total % friend_count
 
 # Display the number of cookies left over
