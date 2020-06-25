cf0c5fb3a78c82e3b8792ce5ece6050630893fd5
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-06-24 22:52:11 -0400

    Update cookies.py

--- 1_beginner/chapter3/solutions/cookies.py
+++ 1_beginner/chapter3/solutions/cookies.py
@@ -6,7 +6,9 @@ Prompt the user with the following questions:
   How many cookies are burnt?
   How many friends do they have?
 
-Output the number of cookies that will be left over if you throw out all the burnt cookies, and all the friends get the same amount.
+Output the number of cookies that will be left over 
+if you throw out all the burnt cookies, 
+and all the friends get the same amount.
 """
 
 # Get all the inputs and store as integers in variables
