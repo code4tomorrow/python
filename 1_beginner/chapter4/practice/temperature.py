5fe75d01ebca8797ce43701838ec35c96593b681
Rebecca Dang <ph.rdang@gmail.com>
2020-06-26 14:34:51 -0700

    Add Temperature problem (from Nono H.'s patch-2 branch)

---
+++ 1_beginner/chapter4/practice/temperature.py
@@ -0,0 +1,20 @@
+"""
+Temperature
+Hoppity the Rabbit wrote some code,
+but it doesn't run correctly and he needs your help!
+The following program is supposed to print a string
+based on a numerical input of the temperature.
+See if you can fix it for him!
+"""
+
+# I added some comments to aid you. Good luck! - Hoppity
+
+# assume possible input range is 0-100
+temp = int(input("Enter temperature: "))
+
+if temp < 100:  # 60-100 is hot
+    print('hot')
+if temp <= 60:  # 30-59 is normal
+    print('normal')
+if temp < 30:  # 0-29 is cold
+    print('cold')
---
+++ 1_beginner/chapter4/solutions/temperature.py
@@ -0,0 +1,26 @@
+"""
+Temperature
+Hoppity the Rabbit wrote some code,
+but it doesn't run correctly and he needs your help!
+The following program is supposed to print a string
+based on a numerical input of the temperature.
+See if you can fix it for him!
+"""
+
+# assume possible input range is 0-100
+temp = int(input("Enter temperature: "))
+
+if 60 <= temp <= 100:  # 60-100 is hot
+    print('hot')
+if 30 <= temp < 60:  # 30-59 is normal
+    print('normal')
+if 0 <= temp < 30:  # 0-29 is cold
+    print('cold')
+
+# alternatively, you can also use elif
+# if temp >= 60:
+#     print('hot')
+# elif temp >= 30:
+#     print('normal')
+# else:
+#     print('cold')
