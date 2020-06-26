4fd2f25e9971264bb6bad2e2a926932dd8dd922f
Nono <38902034+BitPupper@users.noreply.github.com>
2020-06-26 11:21:24 +0900

    Create monthandday.py

---
+++ 1_beginner/chapter4/practice/monthandday.py
@@ -0,0 +1,7 @@
+"""
+Write a program that takes user input of the month and day, 
+and prints out "Boo!" if it is Halloween, and "April fools!" if it is April fools' day.
+Assume all inputs are integers.
+"""
+
+#Your code goes here
