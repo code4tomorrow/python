b0ec9bacb02b430224a3467d30d6691b2293dfc7
Rebecca Dang <ph.rdang@gmail.com>
2020-06-25 22:12:47 -0700

    Clarify Month and Day instructions and add its solution

--- 1_beginner/chapter4/practice/month_and_day.py
+++ 1_beginner/chapter4/practice/month_and_day.py
@@ -1,7 +1,9 @@
 """
-Write a program that takes user input of the month and day,
+Write a program that takes user input of the month and date,
 and prints out "Boo!" if it is Halloween, and "April fools!"
-if it is April fools' day. Assume all inputs are integers.
+if it is April fools' day. If it is any other month and date,
+print "It's not Halloween or April Fools..."
+Assume all inputs are integers (for example, month 1 is January).
 """
 
 # Your code goes here
