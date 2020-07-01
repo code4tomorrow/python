5e3dfa3ae4eea6604299945cbbb3ced50c131cc6
Rebecca Dang <ph.rdang@gmail.com>
2020-07-01 14:18:44 -0700

    Clarify Float instructions to specify input is a float

--- 1_beginner/chapter2/solutions/float.py
+++ 1_beginner/chapter2/solutions/float.py
@@ -1,8 +1,10 @@
 # Float
-# Write a program that takes a number from the user
+# Write a program that takes a float from the user
 # and stores it in a variable. Cast the number to an
 # integer and store in another variable.
-# Then print: (floating point number) = (integer).
+# Then print: (floating point number) = (integer number).
+# For example, if the user enters 5, the output would be:
+# 5.0 = 5
 # Also print the type of the floating point variable.
 # Remember! type(variable_name) will return the data type of a variable
 
