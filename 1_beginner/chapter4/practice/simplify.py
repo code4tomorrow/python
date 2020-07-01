f4670a7e7ea7c0fb2cf9e1d3093aaaa2abc52fe1
Anant Bhatia <anant.x.bhatia@gmail.com>
2020-06-30 19:58:36 -0700

    Commiting everything

--- 1_beginner/chapter4/practice/simplify.py
+++ 1_beginner/chapter4/practice/simplify.py
@@ -1,10 +1,12 @@
-''' Here is a block of code where one tries do determine if
-somone is a nobel prize winner. Rewrite the code to work in one statement'''
+"""Here is a block of code where one tries do determineif somone is a nobel prize winner.
+
+Rewrite the code to work in one statement
+"""
 
 if (doesSignificantWork):
     if (makesBreakthrough):
-		nobelPrizeCandidate = True
-	else:
-		nobelPrizeCandidate = False
-elif(not doesSignificantWork)
-	nobelPrizeCandidate = False
+        nobelPrizeCandidate = True
+    else:
+        nobelPrizeCandidate = False
+elif(not doesSignificantWork):
+    nobelPrizeCandidate = False
