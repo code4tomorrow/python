732c56ba866f103b21c964bc727841d6add2e521
Rebecca Dang <ph.rdang@gmail.com>
2020-07-01 14:19:58 -0700

    Fix typo in No Greater Than instructions

--- 1_beginner/chapter3/practice/no_greater_than.py
+++ 1_beginner/chapter3/practice/no_greater_than.py
@@ -4,7 +4,7 @@ as an input and checks if it is greater than 100.
 Print True if it is, and False if it isn't
 
 YOU MAY NOT USE THE GREATER THAN or
-LESS THAN OPERATOR (>, <, >=, or ,=).
+LESS THAN OPERATORS (>, <, >=, or <=).
 Find a way to do this problem only
 using only the == operator and any math operators you want.
 '''
