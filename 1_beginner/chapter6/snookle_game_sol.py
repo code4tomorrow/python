64fbafa1987fd05091bdc6ee1c72ee7d333068d2
Nono <38902034+BitPupper@users.noreply.github.com>
2020-07-01 12:27:06 +0900

    Update snookle_game_sol.py

--- 1_beginner/chapter6/snookle_game_sol.py
+++ 1_beginner/chapter6/snookle_game_sol.py
@@ -1,3 +1,17 @@
+"""
+Snookle Game
+Snookle the sheep wants to play a game. 
+Given a list of positive integers and a main number, the player iterates 
+through each element in the list and chooses to either add it or 
+to subtract it from the current main number. 
+This is done by having the user enter either 'add' or 'subtract' every turn.
+The main number will be updated to the new value.
+A player wins if they make 12 to be the main number.
+If the end of the list is reached, go back to the first element 
+in the list and keep going until the player wins.
+Code the game for Snookle! 
+"""
+
 nums = [3,1,4,2,6,5,8,10]
 main = 7
 
