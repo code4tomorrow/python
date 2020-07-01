c79bc8b32d6b4678b4c7b284ea7eafe3805fa981
Rebecca Dang <ph.rdang@gmail.com>
2020-06-30 21:20:56 -0700

    Fix Integer Info style
    Shortened instruction lines according to flake8,
    and instead of creating and printing the list in 1 line,
    I separated the steps to make it a bit more readable.

--- 1_beginner/chapter6/solutions/integer_info.py
+++ 1_beginner/chapter6/solutions/integer_info.py
@@ -1,14 +1,21 @@
 """
 Integer Info
-Create a program that takes an integer as input and creates a list with the following elements:
+Create a program that takes an integer as input and
+creates a list with the following elements:
     The number of digits
     The last digit
     A 'True' boolean value if the number is even, 'False' if odd
-Print the list. 
+Print the list.
 Some examples are given to help check your work.
 """
-#Example 1: The input 123456 should print [6, 6, True]
-#Example 2: The input 101202303 should print [9, 3, False]
 
-num = int(input())  #convert string input to int
-print([len(str(num)), num % 10, (num % 2 == 0)]) #taking the modulo of 10 of any number will return its last digit
+# Example 1: The input 123456 should print [6, 6, True]
+# Example 2: The input 101202303 should print [9, 3, False]
+
+num = int(input("Enter an integer: "))  # convert string input to int
+info = [
+    len(str(num)),
+    num % 10,  # mod 10 of any number will return its last digit
+    num % 2 == 0
+]
+print(info)
