eaebb0dcad678f717b07f6d5b7833db73128e93f
Rebecca Dang <ph.rdang@gmail.com>
2020-06-30 21:16:05 -0700

    Clarified Too Long instructions
    The way the instructions were written before, at first
    I thought we were removing elements until the list
    was less than length 4, but when I looked at the code
    carefully I realized we're removing elements
    with length greater than 4.

--- 1_beginner/chapter6/practice/too_long.py
+++ 1_beginner/chapter6/practice/too_long.py
@@ -1,6 +1,6 @@
 """
 Too Long
-Print and remove all elements in a given list with length greater than 4.
+Print and remove all elements with length greater than 4 in a given list.
 """
-#list to help you test your code
+# list to help you test your code
 the_list = ['dragon', 'cab', 'science', 'dove', 'lime', 'river', 'pop']
--- 1_beginner/chapter6/solutions/too_long.py
+++ 1_beginner/chapter6/solutions/too_long.py
@@ -1,10 +1,10 @@
 """
 Too Long
-Print and remove all elements in a given list with length greater than 4.
+Print and remove all elements with length greater than 4 in a given list.
 """
 the_list = ['dragon', 'cab', 'science', 'dove', 'lime', 'river', 'pop']
 
-for x in the_list:  #iterates through every element in the_list
-    if(len(x) > 4): #if length is greater than 4
-        print(x)    #print it
-        the_list.remove(x)
+for x in the_list:  # iterates through every element in the_list
+    if len(x) > 4:  # if element length is greater than 4
+        print(x)  # prints element
+        the_list.remove(x)  # removes element
