e41df666175974388117ac429511bf53e00a2243
Rebecca Dang <ph.rdang@gmail.com>
2020-07-04 15:48:06 -0700

    Add membership operators example code

---
+++ 1_beginner/chapter6/examples/membership_operators.py
@@ -0,0 +1,13 @@
+# Membership Operators
+
+my_list = [1, 2, 3, "oh no"]
+
+if "oh no" in my_list:
+    print("oh no is an element in my_list")
+else:
+    print("oh no is not an element in my_list")
+
+if 4 not in my_list:
+    print('4 is not an element in my_list')
+else:
+    print('4 is an element in my_list')
