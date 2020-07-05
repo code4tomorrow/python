04a6ff3d5623b802e0db75e45d6f50d84151facb
Rebecca Dang <ph.rdang@gmail.com>
2020-07-05 10:51:07 -0700

    Add another explanation for Why Dictionaries
    Before, the only explanation for using dicts were an alternative to using a much of elifs,
    but a better reason is to avoid using parallel lists.
    Dictionaries allow us to associate a key with a value, and that's the main
    reason why they're useful.

---
+++ 2_intermediate/chapter9/examples/why_dict.py
@@ -0,0 +1,27 @@
+# Why Dictionaries
+
+# dictionaries as an alternate to parallel lists
+names = ['Jane Doe', 'John Williams', ]
+addresses = ['1234 Main St', '5678 Market Pl', ]
+
+# better solution: make a dictionary to
+# explicitly associate a name with an address
+# this is also called mapping a key to a value
+contacts = {
+    'Jane Doe': '1234 Main St',
+    'John Williams': '5678 Market Pl',
+    'Alex Summers': '1357 Wall St'
+}
+
+# implementation 1 using if statements
+value = 10
+if value == 10:
+    print("Tom")
+elif value == 20:
+    print("Daniel")
+elif value == 30:
+    print("Elizabeth")
+
+# implementation 2 using dictionary
+dict1 = {10: "Tom", 20: "Daniel", 30: "Elizabeth"}
+print(dict1[value])
