32c320a89eb47015682ecad5cce4e2c4bd368b90
Rebecca Dang <ph.rdang@gmail.com>
2020-07-04 20:20:43 -0700

    Add Sum List exercise

---
+++ 1_beginner/chapter6/practice/sum_list.py
@@ -0,0 +1,8 @@
+# Sum List
+# Create a program that prints the
+# sum of numbers in a list.
+# A test list has been given to you.
+
+numbers = [34, 12, 57, 0, 325, -2535, 12, 1]
+
+# write code here
---
+++ 1_beginner/chapter6/solutions/sum_list.py
@@ -0,0 +1,13 @@
+# Sum List
+# Create a program that prints the
+# sum of numbers in a list.
+# A test list has been given to you.
+
+numbers = [34, 12, 57, 0, 325, -5, 12, 1]
+
+sum = 0
+
+for num in numbers:
+    sum += num
+
+print('Sum of elements in list:', sum)
