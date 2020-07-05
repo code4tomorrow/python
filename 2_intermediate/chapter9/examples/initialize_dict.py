7a47c4d3f9212e14f4530d808c2bf8a0283ff4ef
Rebecca Dang <ph.rdang@gmail.com>
2020-07-05 12:23:22 -0700

    Change inner iterable from a set to a list
    Since students haven't learned sets,
    it's better to just make the inner iterable
    a list instead.

--- 2_intermediate/chapter9/examples/initialize_dict.py
+++ 2_intermediate/chapter9/examples/initialize_dict.py
@@ -7,7 +7,7 @@ contacts = {
 }
 print(contacts)
 
-# creating a dict from a list
-my_list = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
+# creating a dict from a list of lists
+my_list = [["key1", "value1"], ["key2", "value2"], ["key3", "value3"]]
 my_dict = dict(my_list)
 print(my_dict)
