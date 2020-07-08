724c8bfb019277814b204ab0d90381b1c259248a
Rebecca Dang <ph.rdang@gmail.com>
2020-07-08 11:23:28 -0700

    Fix instructions in TV exercise to match what's on Thinkific

--- 1_beginner/chapter5/solutions/TV.py
+++ 1_beginner/chapter5/solutions/TV.py
@@ -1,14 +1,14 @@
 # TV
-# Pretend you just got a 37 on your test,
-# and you mom says you can’t watch TV until you get above an 84.
-# (HINT: comparison operators). You increase
-# your test score 6 points per day (iterations).
+# Pretend you just got a 50 on your test,
+# and you mom says you can’t watch TV until you get
+# a score of at least 80. (HINT: comparison operators).
+# You increase your test score by 10 points per day.
 # Write a program that tells you after
-# how many days you'll be able to watch TV
+# how many days you'll be able to watch TV. Use a loop.
 
-x = 37
-d = 0
-while x < 84:
-    x += 6
-    d += 1
-print(d)
+x = 50
+days = 0
+while x < 80:
+    x += 10
+    days += 1
+print("You can watch TV after", days, "days")
