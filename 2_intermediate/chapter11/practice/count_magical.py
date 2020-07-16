8330f3f900ac0bb18319dbb41bc9fa35037bb75d
Rebecca Dang <ph.rdang@gmail.com>
2020-07-15 20:00:27 -0700

    Add main function to test count_magical

--- 2_intermediate/chapter11/practice/count_magical.py
+++ 2_intermediate/chapter11/practice/count_magical.py
@@ -4,5 +4,10 @@
 # if the number of evens is greater than
 # half of the length of the list, print "Magical"
 # Else, print "Not Magical"
+#
+# Write a function called main which tests
+# the count_magical function on at least
+# 3 different lists of integers. Use the main
+# function to test count_magical by calling main().
 
 # write code here
--- 2_intermediate/chapter11/solutions/count_magical.py
+++ 2_intermediate/chapter11/solutions/count_magical.py
@@ -4,6 +4,11 @@
 # if the number of evens is greater than
 # half of the length of the list, print "Magical"
 # Else, print "Not Magical"
+#
+# Write a function called main which tests
+# the count_magical function on at least
+# 3 different lists of integers. Use the main
+# function to test count_magical by calling main().
 
 def count_magical(my_list):
     number_of_evens = 0
@@ -17,3 +22,16 @@ def count_magical(my_list):
         print("Not Magical")
 
     return number_of_evens
+
+
+def main():
+    list_1 = [1, 2, 3, 4, 5, 6]  # not magical, 3 evens
+    list_2 = [0, 35, 1, 35, 2, 4]  # not magical, 3 evens
+    list_3 = [10, 20, 12, 3, -9]  # magical, 3 evens
+
+    print("Number of evens in list 1:", count_magical(list_1))
+    print("Number of evens in list 2:", count_magical(list_2))
+    print("Number of evens in list 3:", count_magical(list_3))
+
+
+main()
