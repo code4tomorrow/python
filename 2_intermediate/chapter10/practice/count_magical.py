b9a9276d25851d909a7fc52fea89ee53defc4fc9
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-07-15 23:01:01 -0400

    Create count_magical.py

---
+++ 2_intermediate/chapter10/practice/count_magical.py
@@ -0,0 +1,3 @@
+#Write a function that calculates the number of even numbers in a list
+#In the function, if the number of evens is greater than half the list, print "Magical"
+#Else, print "Not Magical"
