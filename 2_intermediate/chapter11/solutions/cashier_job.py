e640280e0e8dd26639ee783309c4388c7d9bd032
Lint Action <lint-action@samuelmeuli.com>
2020-07-18 18:42:00 +0000

    Fix code style issues with Black

--- 2_intermediate/chapter11/solutions/cashier_job.py
+++ 2_intermediate/chapter11/solutions/cashier_job.py
@@ -1,17 +1,21 @@
 # Write a function that will take the number of pennies, nickels, dimes,
 # quarters, and discount(i.e. 15 for 15% discount). Return the total amount of
-# money after discount. 
+# money after discount.
 #
 # Print what is returned by the function after it is run with 97 pennies,
 # 13 nickels, 18 dimes, 54 quarters, and 20% discount.
 # Print what is returned by the function after it is run with 32 pennies,
 # 19 nickels, 22 dimes, 71 quarters, and 51% discount.
 
-def calc_total(penny, nickel, dime, quarter, discount):
-    before_discount = 0.01*penny + 0.05*nickel + 0.1*dime + 0.25*quarter
-    discount_multiplier = 1 - discount*0.01
 
-    return round(before_discount* discount_multiplier, 2)
+def calc_total(penny, nickel, dime, quarter, discount):
+    before_discount = (
+        0.01 * penny + 0.05 * nickel + 0.1 * dime + 0.25 * quarter
+    )
+    discount_multiplier = 1 - discount * 0.01
+
+    return round(before_discount * discount_multiplier, 2)
+
 
 print(calc_total(97, 13, 18, 54, 20))
 
