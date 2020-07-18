edc7c2879ec6ceb16aa8ead4fd0c995f2aa5feff
Rebecca Dang <ph.rdang@gmail.com>
2020-07-18 15:42:41 -0700

    Copy instructions from num mystery 1 solution

--- 2_intermediate/chapter11/practice/number_mystery_1.py
+++ 2_intermediate/chapter11/practice/number_mystery_1.py
@@ -1,6 +1,12 @@
-# Write a function that takes in 3 integers. Get the sum of the 3 integers. Get
-# the difference between the largest integer and the smallest integer. The
-# function will return the product of these two integers you got.
+# Number Mystery 1
+# Write a function called num_mystery that takes in 3 integers.
+# The function should calculate the sum of the 3 integers and
+# the difference between the largest integer and the smallest integer.
+# The function should return the product of these two integers you calculated.
 #
-# Use this function on 1,2,3 and print it. Use this function on 5,13,7 and
-# print it
+# Hint: You may find it useful to use the max() and min() functions.
+#
+# Use the num_mystery function on 1, 2, 3 and print the result.
+# Use the num_mystery function on 5, 13, 7 and print the result.
+
+# write code here
