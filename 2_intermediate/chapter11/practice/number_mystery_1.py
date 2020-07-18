b7b1baad244756cbadf89d356d9b34e15f9afa5f
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-07-18 14:09:13 -0400

    Add files via upload

---
+++ 2_intermediate/chapter11/practice/number_mystery_1.py
@@ -0,0 +1,6 @@
+# Write a function that takes in 3 integers. Get the sum of the 3 integers. Get
+# the difference between the largest integer and the smallest integer. The
+# function will return the product of these two integers you got.
+#
+# Use this function on 1,2,3 and print it. Use this function on 5,13,7 and
+# print it
