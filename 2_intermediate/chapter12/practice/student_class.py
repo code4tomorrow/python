01c281f7e369d8005ac61b1c3310e1af0c53d38f
Rebecca Dang <ph.rdang@gmail.com>
2020-07-19 22:47:54 -0700

    Update instructions and fix method names

--- 2_intermediate/chapter12/practice/student_class.py
+++ 2_intermediate/chapter12/practice/student_class.py
@@ -1,5 +1,5 @@
-# Create a class called Student with instance attributes: Name and Age.
-# The user can input the Name and Age. Add 2 methods to the class:
-# A raiseHand method which prints out the student name followed
-# by “is now raising their hand.” A growOlder method that makes the
-# student older by 1 year.
+# Create a class called Student with instance attributes: name and age.
+# The user can input the name and age. Add 2 methods to the class:
+# 1. A raise_hand method which prints out the student's name followed
+# by "is now raising their hand."
+# 2. A grow_older method that makes the student older by 1 year.
--- 2_intermediate/chapter12/solutions/student_class.py
+++ 2_intermediate/chapter12/solutions/student_class.py
@@ -1,8 +1,8 @@
-# Create a class called Student with instance attributes: Name and Age.
-# The user can input the Name and Age. Add 2 methods to the class:
-# A raiseHand method which prints out the student name followed
-# by “is now raising their hand.” A growOlder method that makes the
-# student older by 1 year.
+# Create a class called Student with instance attributes: name and age.
+# The user can input the name and age. Add 2 methods to the class:
+# 1. A raise_hand method which prints out the student's name followed
+# by "is now raising their hand."
+# 2. A grow_older method that makes the student older by 1 year.
 
 
 class Student:
@@ -10,8 +10,8 @@ class Student:
         self.name = name
         self.age = age
 
-    def raiseHand(self):
+    def raise_hand(self):
         print(self.name + " is now raising their hand.")
 
-    def growOlder(self):
+    def grow_older(self):
         self.age += 1
