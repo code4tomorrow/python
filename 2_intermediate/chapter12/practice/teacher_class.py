7cde5061492b34189cdee13d8c0150056c7e8e0d
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-07-19 23:11:56 -0400

    Add teacher

---
+++ 2_intermediate/chapter12/practice/teacher_class.py
@@ -0,0 +1,21 @@
+# Create a class called Teacher. Add 3 variables to this
+# class: Name, Age, and an array of Student classes from student_class.
+# The user can input these 3 variables. Add 2 Methods to the class: A
+# displayClass method that prints out the names of all the students.
+# A graduate method that increments the age of all of his/her students by 1.
+# Then prints out all the ages.
+
+# Student class implemented below. Teacher class uses it.
+
+class Student
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def raiseHand(self):
+        print(self.name + " is now raising their hand.")
+
+    def growOlder(self):
+        self.age += 1
+
