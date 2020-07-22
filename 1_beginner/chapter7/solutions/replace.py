24839cedba99a92a3a38048ad573979a28904d43
Rebecca Dang <ph.rdang@gmail.com>
2020-07-22 10:40:01 -0700

    Clarify Replace instructions

--- 1_beginner/chapter7/solutions/replace.py
+++ 1_beginner/chapter7/solutions/replace.py
@@ -1,13 +1,14 @@
 """
 Replace
 
-Write a Python program to print a string
-from a given string where all occurrences
-of its first char have been changed to '$',
+Write a Python program that asks the user for a string
+and then prints a version of that string
+where all occurrences of its first char
+have been changed to '$',
 except the first char itself.
 
-Sample String: 'restart'
-Expected Result: 'resta$t'
+Sample Input: 'restart'
+Expected Output: 'resta$t'
 
 Adapted from W3Resource, problem 4:
 https://www.w3resource.com/python-exercises/string/
