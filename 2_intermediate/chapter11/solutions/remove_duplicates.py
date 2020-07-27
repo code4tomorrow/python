4d26c4a87029e9a77a64bbf4314a01cd0ce207d8
Srikar Eranky <59518247+srikar-eranky@users.noreply.github.com>
2020-07-26 21:09:49 -0700

    Update remove_duplicates.py
    Redid the solution using dictionaries, didn't realize that they were covered in Ch.9. If you plan on teaching Ch.11 before Ch.9, highly recommend that you don't use this problem.

--- 2_intermediate/chapter11/solutions/remove_duplicates.py
+++ 2_intermediate/chapter11/solutions/remove_duplicates.py
@@ -3,10 +3,11 @@
 # The function should look through a list,
 # Find all duplicate elements, and remove them
 # Sort the resulting list
-# YOU MAY NOT USE THE set() function IN PYTHON. Use this problem to practice list iteration!
+# YOU MAY NOT USE THE set() function IN PYTHON.
 # Hint: To sort a list, use sorted(list)
-# Another hint: Use list.count(element)
-# To count the number of times that element appears
+# Another hint: Use dict.fromkeys(list)
+# To take the elements from a list,
+# and convert them to keys in a dictionary
 
 # Example: array = [1,1,2,5,4,6,12,3,4,6]
 # Result should print [1,2,3,4,5,6,12]
@@ -16,17 +17,16 @@
 list1 = [1, 1, 2, 5, 4, 6, 12, 3, 4, 6]  # Define your list
 
 
-def remove_duplicate(array):
-    for i in array:
-        # Checks if element appears multiple times
-        for j in range(i, len(array) - 1):
-            #  Counts the number of times an element appears.
-            if array.count(array[i]) == 1:
-                break  # If it appears once, break out of the loop
-            else:
-                # If it appears more than once, remove it
-                array.remove(array[i])
-    return sorted(array)  # Sorts the list
+# Define your Function
+def remove_duplicates(array):
+    my_list = list(dict.fromkeys(array))
+    # Converts the list into a dictionary.
+    # Fromkeys(array) turns each item into a key
+    # There cannot be multiple keys,
+    # So all the duplicate keys are removed
+    # Convert the keys back into a list
+    return sorted(my_list)
+    # Returns the sorted list of keys that are not duplicate.
 
 
-print(remove_duplicate(list1))  # Call the function
+print(remove_duplicates(list1))  # Call the function
