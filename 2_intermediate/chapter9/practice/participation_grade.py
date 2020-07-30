19759bfe23aaf9a2ab7c96bb2a25177d4010b552
Lint Action <lint-action@samuelmeuli.com>
2020-07-30 15:29:49 +0000

    Fix code style issues with Black

--- 2_intermediate/chapter9/practice/participation_grade.py
+++ 2_intermediate/chapter9/practice/participation_grade.py
@@ -4,7 +4,7 @@
 # name in the list is the number of times the student participated in this week.
 # If a student has more the 7 participation, then got an A. If a student has
 # more than 3 but less than 8, the student got a B. If a student has more than
-# 0 but less than 4, the student got a C. 
+# 0 but less than 4, the student got a C.
 #
 # Make a dictionary with the keys as the students' name and the values as the
 # corresponding student's letter grade. Print the dictionary
@@ -12,10 +12,40 @@
 # Write your code below
 
 
-participation_occurences = ["Sam", "Dan", "Bob", "Dan", "Sam", "Sam",
-                            "Bob", "Dan", "Dan", "Ivan", "Ivan",
-                            "Ray", "Sam", "Sam", "Dan", "Ivan", "Ivan",
-                            "Ray", "Dan", "Ivan", "Ivan", "Sam", "Dan",
-                            "Ray", "Sam", "Dan", "Bob", "Dan", "Sam", 
-                            "Sam", "Dan", "Bob", "Dan", "Sam", "Sam"]
-
+participation_occurences = [
+    "Sam",
+    "Dan",
+    "Bob",
+    "Dan",
+    "Sam",
+    "Sam",
+    "Bob",
+    "Dan",
+    "Dan",
+    "Ivan",
+    "Ivan",
+    "Ray",
+    "Sam",
+    "Sam",
+    "Dan",
+    "Ivan",
+    "Ivan",
+    "Ray",
+    "Dan",
+    "Ivan",
+    "Ivan",
+    "Sam",
+    "Dan",
+    "Ray",
+    "Sam",
+    "Dan",
+    "Bob",
+    "Dan",
+    "Sam",
+    "Sam",
+    "Dan",
+    "Bob",
+    "Dan",
+    "Sam",
+    "Sam",
+]
--- 2_intermediate/chapter9/solutions/participation_grade.py
+++ 2_intermediate/chapter9/solutions/participation_grade.py
@@ -4,7 +4,7 @@
 # name in the list is the number of times the student participated in this week.
 # If a student has more the 7 participation, then got an A. If a student has
 # more than 3 but less than 8, the student got a B. If a student has more than
-# 0 but less than 4, the student got a C. 
+# 0 but less than 4, the student got a C.
 #
 # Make a dictionary with the keys as the students' name and the values as the
 # corresponding student's letter grade. Print the dictionary
@@ -12,12 +12,43 @@
 # Write your code below
 
 
-participation_occurences = ["Sam", "Dan", "Bob", "Dan", "Sam", "Sam",
-                            "Bob", "Dan", "Dan", "Ivan", "Ivan",
-                            "Ray", "Sam", "Sam", "Dan", "Ivan", "Ivan",
-                            "Ray", "Dan", "Ivan", "Ivan", "Sam", "Dan",
-                            "Ray", "Sam", "Dan", "Bob", "Dan", "Sam", 
-                            "Sam", "Dan", "Bob", "Dan", "Sam", "Sam"]
+participation_occurences = [
+    "Sam",
+    "Dan",
+    "Bob",
+    "Dan",
+    "Sam",
+    "Sam",
+    "Bob",
+    "Dan",
+    "Dan",
+    "Ivan",
+    "Ivan",
+    "Ray",
+    "Sam",
+    "Sam",
+    "Dan",
+    "Ivan",
+    "Ivan",
+    "Ray",
+    "Dan",
+    "Ivan",
+    "Ivan",
+    "Sam",
+    "Dan",
+    "Ray",
+    "Sam",
+    "Dan",
+    "Bob",
+    "Dan",
+    "Sam",
+    "Sam",
+    "Dan",
+    "Bob",
+    "Dan",
+    "Sam",
+    "Sam",
+]
 
 grade_dict = {}
 for student in participation_occurences:
@@ -29,7 +60,7 @@ for student in participation_occurences:
 for student in grade_dict:
     if grade_dict[student] > 7:
         grade_dict[student] = "A"
-    elif grade_dict[student] >3:
+    elif grade_dict[student] > 3:
         grade_dict[student] = "B"
     else:
         grade_dict[student] = "C"
