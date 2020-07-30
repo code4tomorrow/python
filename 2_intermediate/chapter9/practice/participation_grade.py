fd239e60491a6fbcb01cac93915bc70792fe9f3d
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-07-30 11:27:55 -0400

    Added Participation grade

---
+++ 2_intermediate/chapter9/practice/participation_grade.py
@@ -0,0 +1,21 @@
+# Warning: can be challenging
+#
+# A teacher is given a list of students. The number of occurences of a student's
+# name in the list is the number of times the student participated in this week.
+# If a student has more the 7 participation, then got an A. If a student has
+# more than 3 but less than 8, the student got a B. If a student has more than
+# 0 but less than 4, the student got a C. 
+#
+# Make a dictionary with the keys as the students' name and the values as the
+# corresponding student's letter grade. Print the dictionary
+#
+# Write your code below
+
+
+participation_occurences = ["Sam", "Dan", "Bob", "Dan", "Sam", "Sam",
+                            "Bob", "Dan", "Dan", "Ivan", "Ivan",
+                            "Ray", "Sam", "Sam", "Dan", "Ivan", "Ivan",
+                            "Ray", "Dan", "Ivan", "Ivan", "Sam", "Dan",
+                            "Ray", "Sam", "Dan", "Bob", "Dan", "Sam", 
+                            "Sam", "Dan", "Bob", "Dan", "Sam", "Sam"]
+
