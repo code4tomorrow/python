d00e8533f6ff9afb36d471146631a6e1ad758618
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-08-02 22:18:29 -0400

    Added odd_sum and smooth_max solutions
    Added odd_sum and smooth_max solutions that I created

---
+++ 2_intermediate/chapter10/solutions/odd_sum.py
@@ -0,0 +1,11 @@
+# Odd Sum
+# Given a 2D list, find the sum of all elements at odd indexes for all
+# the lists at odd indexes. Print this sum times the sum of all
+# first element of all the 1D lists in the 2D list.
+#
+# Ex:[[1,2,3,6],[2,41,2,1]]should have print 42 after the program runs.
+#
+# Write the code below.
+
+two_d_list = [[1,2,3,5,2],[2,3,1,4],[2,3,1,2,21],[21,3,1,41]]
+#two_d_list should print 51 after the program runs.
