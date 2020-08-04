f507dd2c6dc294f806ab894bf73cc9d68b6ce241
Rebecca Dang <35876322+phrdang@users.noreply.github.com>
2020-08-04 14:07:25 -0700

    Revert "Kehao ch10problems(2 of them)"

--- 1_beginner/chapter5/practice/alternating.py
+++ 1_beginner/chapter5/practice/alternating.py
@@ -1,5 +1,4 @@
-"""
-Alternating
+""" 
 
 Ask the user for an integer. The print the numbers from 1 to that number,
 but alternating in sign. For example, if the input was 5, what would be printed
@@ -7,6 +6,7 @@ is 1, -1, 2, -2, 3, -3, 4, -4, 5. (Note, DO NOT include the last negative
 number).
 
 Do this with a for loop
+
 """
 
 # Write code here.
--- 2_intermediate/chapter10/practice/img_avg.py
+++ 2_intermediate/chapter10/practice/img_avg.py
@@ -1,31 +1,27 @@
-"""
-Image Average
-
-Here is the challenge problem for 2D loops:
-Images are often represented as 3D arrays,
+""" Here is the challenge problem for 2d loops>
+Images are often represented as 3d arrays,
 where the rows and columns are the pixels in the image,
-and each pixel has an RGB (red, green, blue) value
-which determines the color of the pixel.
+and each pixel has an r, g, and b value.
 
 The interesting thing is that we can iterate over images.
 The challenge is, given an image, create a program that
 will return a different image where each pixel is the average
 of the pixels surrounding it in the original image.
 
-The neighbors of an image are all the pixels that surround it,
-1 on each side, and 4 on the diagonals, for 8 in total. Each
-pixel doesn't necessarily have 8 neighbors, though (think about why).
+The neighbors of an image are all the pixels that surroun it,
+1 on each side, and 4 on the diagonals, for 8 in total. 
+Each pixel doesn't necessarily have 8 neighbors, though (think about why)
 
 The code to grab an image from the internet and make it
 into an array is given to you. The code also displays the new image
 you create in the end.
 
-NOTE: The image is 3 dimensional because each pixel has RGB values.
+NOTE: The image is 3 dimensional because each pixel has rgb values.
 To find the average value of all of a pixels neighbors, you must
 change the average of the red value to the red value, blue to blue, etc.
-For example, if the neighbors of a pixel with value [1, 2, 3]
-were [20, 30, 40] and [10, 120, 30], the new pixel that would replace the
-original one would be [15, 75, 35]
+For example, if the neighbors of a pixel with value [1,2,3]
+were [20,30,40] and [10,120,30], the new pixel that would replace the original one would be
+[15,75,35]
 """
 
 from PIL import Image
