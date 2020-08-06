7eec75c1ca03da6fe3ee6c10d2e1586777076d45
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-08-06 17:51:22 -0400

    Update img_avg.py solution
    Added an example and other changes to improve understanding of the problem. Didn't change the actual solution other than removing the use of functions.

--- 2_intermediate/chapter10/solutions/img_avg.py
+++ 2_intermediate/chapter10/solutions/img_avg.py
@@ -12,82 +12,92 @@ The challenge is, given an image, create a program that
 will return a different image where each pixel is the average
 of the pixels surrounding it in the original image.
 
-The neighbors of an image are all the pixels that surround it,
-1 on each side, and 4 on the diagonals, for 8 in total. Each
-pixel doesn't necessarily have 8 neighbors, though (think about why).
-
-The code to grab an image from the internet and make it
-into an array is given to you. The code also displays the new image
-you create in the end.
-
 NOTE: The image is 3 dimensional because each pixel has RGB values.
 To find the average value of all of a pixels neighbors, you must
 change the average of the red value to the red value, blue to blue, etc.
 For example, if the neighbors of a pixel with value [1, 2, 3]
 were [20, 30, 40] and [10, 120, 30], the new pixel that would replace the
 original one would be [15, 75, 35]
+
+EXAMPLE: A image with,let's say 9 pixels may look like:
+[[[31,41,42], [51,1,101], [24,141,33]],
+ [[50,21,28], [31,49,201], [90,54,33]],
+ [[12,81,3], [22,8,91], [101,141,132]]]
+
+HINT: Don't forget that a pixel may have varying amount of neighboring
+pixels. A pixel at the edge, for example, has 3 neighboring pixels while
+a pixel at the center of the image has 8 neighboring pixels.
+
+GIVEN: The code to grab an image from the internet and make it
+into an array is given to you. The code also displays the new image
+you create in the end. These weren't taught in the main curriculum, so 
+it isn't expected for students to fully understand what the given code 
+does. 
 """
 
+#All the libraries imported 
 from PIL import Image
 import requests
 import numpy
 import matplotlib.pyplot as plt
 
+#Code that grabs the image from the internet and makes it into an array
 url = "https://images.dog.ceo/breeds/waterdog-spanish/20180723_185544.jpg"
-img = numpy.array(Image.open(requests.get(url, stream=True).raw))
+img = numpy.array(Image.open(requests.get(url, stream=True).raw)).tolist()
 newimg = img
 transpose = numpy.transpose(img)
 
-
-# write code to create newimg here
-def solution1():
-    """
-    Iterating over the image here. i is a variable from
-    0 to the width of the image.
-    j is a variable that ranges from 0 to the height of the image.
-    i is associated with values
-    """
-    for i in range(len(img)):
-        for j in range(len(img[0])):
-            x_n = [0]
-            y_n = [0]
-
-            if i == 0:
-                x_n.append(1)
-            elif i == len(img) - 1:
-                x_n.append(-1)
-            else:
-                x_n.append(1)
-                x_n.append(-1)
-
-            if j == 0:
-                y_n.append(1)
-            elif j == len(img[0]) - 1:
-                y_n.append(-1)
-            else:
-                y_n.append(1)
-                y_n.append(-1)
-
-            r_avg = -1 * img[i][j][0]
-            g_avg = -1 * img[i][j][1]
-            b_avg = -1 * img[i][j][2]
-            c = -1
-
-            for x in x_n:
-                for y in y_n:
-                    r_avg += img[i + x][j + y][0]
-                    g_avg += img[i + x][j + y][1]
-                    b_avg += img[i + x][j + y][2]
-                    c += 1
-            r_avg = r_avg / c
-            g_avg = g_avg / c
-            b_avg = b_avg / c
-
-            newimg[i][j] = [r_avg, g_avg, b_avg]
+plt.imshow(img)
+plt.show()
 
 
-solution1()
+#Write code to create newimg here
+"""
+Iterating over the image here. i is a variable from
+0 to the width of the image.
+j is a variable that ranges from 0 to the height of the image.
+i is associated with values
+"""
+for i in range(len(img)):
+    for j in range(len(img[0])):
+        x_n = [0]
+        y_n = [0]
 
+        if i == 0:
+            x_n.append(1)
+        elif i == len(img) - 1:
+            x_n.append(-1)
+        else:
+            x_n.append(1)
+            x_n.append(-1)
+
+        if j == 0:
+            y_n.append(1)
+        elif j == len(img[0]) - 1:
+            y_n.append(-1)
+        else:
+            y_n.append(1)
+            y_n.append(-1)
+
+        r_avg = -1 * img[i][j][0]
+        g_avg = -1 * img[i][j][1]
+        b_avg = -1 * img[i][j][2]
+        c = -1
+
+        for x in x_n:
+            for y in y_n:
+                r_avg += img[i + x][j + y][0]
+                g_avg += img[i + x][j + y][1]
+                b_avg += img[i + x][j + y][2]
+                c += 1
+        r_avg = r_avg / c
+        g_avg = g_avg / c
+        b_avg = b_avg / c
+
+        newimg[i][j] = [r_avg, g_avg, b_avg]
+        
+
+#Code that displays the new image at the end.
 plt.imshow(newimg)
 plt.show()
 
