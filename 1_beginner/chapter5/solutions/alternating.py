992b468e68ba2bcb0601f63be525dab8ccd43ea1
Rebecca Dang <ph.rdang@gmail.com>
2020-08-07 11:00:42 -0700

    Fix style alternating.py

--- 1_beginner/chapter5/solutions/alternating.py
+++ 1_beginner/chapter5/solutions/alternating.py
@@ -4,25 +4,24 @@ Ask the user for an integer. The print the numbers from 1
 to that number, but alternating in sign. For example, if the input
 was 5, what would be printed is 1, -1, 2, -2, 3, -3, 4, -4, 5.
 (Note, DO NOT include the last negative number).
-Do this with a for loop
+Do this with a for loop and then with a while loop.
 """
 
-# Write code here.
-
-number = int(input("Enter Number Here: "))
+# for loop solution
+number = int(input("Enter number here: "))
 for num in range(1, number + 1):
     if num == number:
         print(num)
     else:
         print(num)
-        print(-1 * num)
+        print(-num)
 
 
-# Now try it with a while loop
-number = int(input("Enter Number Here: "))
+# while loop solution
+number = int(input("Enter number here: "))
 current_num = 1
 while current_num < number:
     print(current_num)
-    print(-1 * current_num)
+    print(-current_num)
     current_num += 1
 print(current_num)
