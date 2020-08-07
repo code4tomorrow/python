365dbd3a752d17e9724c19753db1ba97e540fb73
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-08-06 21:01:25 -0400

    Made the characters per line less in alternating
    Made the characters per line less in practice/alternating

--- 1_beginner/chapter5/practice/alternating.py
+++ 1_beginner/chapter5/practice/alternating.py
@@ -1,10 +1,12 @@
 """
 Alternating
+Ask the user for an integer. The print the numbers from 1
+to that number, but alternating in sign. For example, if the input
+was 5, what would be printed is 1, -1, 2, -2, 3, -3, 4, -4, 5.
+(Note, DO NOT include the last negative number).
+Do this with a for loop
+"""
 
-Ask the user for an integer. The print the numbers from 1 to that number,
-but alternating in sign. For example, if the input was 5, what would be printed
-is 1, -1, 2, -2, 3, -3, 4, -4, 5. (Note, DO NOT include the last negative
-number).
 
 Do this with a for loop
 """
