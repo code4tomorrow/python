1ea376366049c44d2b0bc9b4464f3c3f8ed126a4
Lint Action <lint-action@samuelmeuli.com>
2020-09-13 20:29:33 +0000

    Fix code style issues with Black

--- 3_advanced/chapter14/practice/even_words.py
+++ 3_advanced/chapter14/practice/even_words.py
@@ -1,9 +1,9 @@
-#Problem name: even_words
-#Given a string that the user inputs create a list that contains the square of the lengths of words with an even amount of characters. 
-#For example, if the string is “I am cool”, the list would be [4, 16]. 
-#Use a list comprehension to solve it.
+# Problem name: even_words
+# Given a string that the user inputs create a list that contains the square of the lengths of words with an even amount of characters.
+# For example, if the string is “I am cool”, the list would be [4, 16].
+# Use a list comprehension to solve it.
 
-#the user inputs the string
+# the user inputs the string
 string = input()
 
-#write your code below
+# write your code below
--- 3_advanced/chapter14/solutions/odd_squares.py
+++ 3_advanced/chapter14/solutions/odd_squares.py
@@ -1,13 +1,12 @@
-#Problem name: odd_squares
-#Given a list of integers, create a list of all the squares of the odd integers within the list. 
-#Use a list comprehension to solve it.
+# Problem name: odd_squares
+# Given a list of integers, create a list of all the squares of the odd integers within the list.
+# Use a list comprehension to solve it.
 
-#the given code takes an input and makes it a list of numbers
-#for example, entering “1 23 4” as the input will result in the list [1,23,4]
+# the given code takes an input and makes it a list of numbers
+# for example, entering “1 23 4” as the input will result in the list [1,23,4]
 ex_list = input().split()
 for idx in range(len(ex_list)):
-	ex_list[idx] = int(ex_list[idx])
+    ex_list[idx] = int(ex_list[idx])
 
-#write your code below
-odds_quares = [n**2 for n in list if n%2 == 1]
-
+# write your code below
+odds_quares = [n ** 2 for n in list if n % 2 == 1]
