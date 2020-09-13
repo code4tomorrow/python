6fb83fa3a323ff6db8b31facee20457a10af70e3
Priyanka Karunakaran <ashwin@Priyankas-MacBook-Pro.local>
2020-09-13 13:09:55 -0700

    adding Ch. 14 practice and solutions

--- 3_advanced/chapter14/solutions/square_root_list.py
+++ 3_advanced/chapter14/solutions/square_root_list.py
@@ -1,8 +1,8 @@
+
 #Problem Name: square_root_list
 #Take a user given list of numbers and make a list of all the square roots of the numbers using list comprehension. 
 #Use a list comprehension to solve it. 
 #Hint: The square root of a number is the same as taking the ½ power of a number. 
-
 #the given code takes an input and makes it a list of numbers
 #for example, entering “1 23 4” as the input will result in the list [1,23,4]
 ex_list = input().split()
@@ -10,3 +10,4 @@ for idx in range(len(ex_list)):
 	ex_list[idx] = int(ex_list[idx])
 
 #write your code below
+new_list =  [number**(½) for number in ex_list]
