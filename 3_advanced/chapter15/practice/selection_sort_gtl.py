a0b4be5bf653caeb101a6af17d891dbdd9f7c89a
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-09-19 12:04:20 -0400

    Added the practice templates for 3 selection sort problems.
    Added the practice templates for 3 selection sort problems.

---
+++ 3_advanced/chapter15/practice/selection_sort_gtl.py
@@ -0,0 +1,17 @@
+"""
+The Selection Sort code we saw sorts an array from least to greatest.
+Modify the code so that the code sorts an array from greatest to least. 
+
+Selection Sort Code:
+
+arr  = [?,?,?]#change this array to the array you want to sort
+for first_idx in range(len(arr)):
+    min_idx = first_idx
+    for second_idx in range(first_idx+1, len(arr)):
+        if arr[second_idx] < arr[min_idx]:
+            min_idx = second_idx
+    arr[first_idx], arr[min_idx] = arr[min_idx], arr[first_idx]
+
+"""
+
+#write your code below
