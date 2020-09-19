b8c50bf3e9aa1375f803d0ee9c771fe7cba920be
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-09-19 12:34:25 -0400

    Fixed Lint errors

--- 3_advanced/chapter14/solutions/worried_josh.py
+++ 3_advanced/chapter14/solutions/worried_josh.py
@@ -1,14 +1,23 @@
-#Problem Name: worried_josh
+# Problem Name: worried_josh
 
-#Josh is worried about his test score. He wants to score in the top n, where n is a positive integer that the user inputs. Given a list of student names where the student with the highest score is the 0th index and the score goes down from there, print “YES!” if Josh scores in the top n, and “NO :(“ if he doesn’t. Assume n will not be greater than the number of students. Use enumerate to solve this problem.
+"""
 
+Josh is worried about his test score. He wants to score in the top n, 
+where n is a positive integer that the user inputs. Given a list of 
+student names where the student with the highest score is the 0th index 
+and the score goes down from there, print “YES!” if Josh scores in the top n, 
+and “NO :(“ if he doesn’t. Assume n will not be greater than the number of students. 
+Use enumerate to solve this problem.
 
-#the list of student names is given and the n is a user input
-#remember the leftmost student has the highest score whereas the rightmost has the lowest score
+"""
+
+# the list of student names is given and the n is a user input
+# remember the leftmost student = highest score, rightmost student = lowest score
 students = [“Dan”, “Sherlocks”, “Jo”, “Josh”, “Dennis”, “Erwin”, “Ivan”, “Penny”] 
 n = int(input())
 
-#write your code below
+# write your code below
+
 said_yes = False
 for index, name in enumerate(students):
 	if name == “Josh” and index +1 <= n:
