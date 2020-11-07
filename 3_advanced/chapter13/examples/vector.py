78ca7af1755a9caea0f0f42891709be423f55052
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-07 17:27:23 -0500

    Added Vector class

---
+++ 3_advanced/chapter13/examples/vector.py
@@ -0,0 +1,21 @@
+class Vector:
+	"""
+	Constructor
+ 
+	self: a reference to the object we are creating
+	vals: a list of integers which are the contents of our vector
+	"""
+	def __init__(self, vals):
+		self.vals = vals # Notice! We are using the keyword self to create a field or property (for Javascript users) 
+		print("Assigned values ", vals, " to vector.")
+ 
+	"""
+	String Function
+ 
+	Converts the object to a string in readable format for programmers
+	"""
+	def __str__(self):
+		return str(self.vals) # Returns the contents of the vector
+ 
+vec = Vector([2, 3, 2])
+print(str(vec)) # [2, 3, 2]
