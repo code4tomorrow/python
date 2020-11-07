22b9f1500a1759802d6e0889c60485a800373ce4
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-07 18:37:56 -0500

    Added chapter sections 13.1 to 13.4 practice problems template

---
+++ 3_advanced/chapter13/practice/triangle.py
@@ -0,0 +1,16 @@
+"""
+Write a class called Triangle which will take three tuples
+(each tuple contains two integers: the x and y coordinates
+of a vertex). Then, define an __add__ operation that acts as
+a translation operation. Its input argument will be a tuple
+of two integers that will indicate the x and y translations
+that will be applied to each coordinate. (basically, add the
+tuple to each coordinate of the triangle). Also, define a
+vertical and horizontal transformation tool in the form
+of __mul__ which will also take a tuple of two integers that
+will be multiplied to the x and y coordinates of each vertex
+respectively.
+"""
+
+# write your code below
+
