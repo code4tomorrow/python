24d11f10632375ac088e4c522d1b7d8965aab443
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-09 22:41:19 -0500

    Update matrix_add_subtract.py

--- 3_advanced/chapter13/solutions/matrix_add_subtract.py
+++ 3_advanced/chapter13/solutions/matrix_add_subtract.py
@@ -13,16 +13,16 @@ Matrix class code will be given.
 This is the unmodified Matrix class code.
 
 class Matrix:
- def __init__(self,thelist: list):
-   self.thelist=thelist
-   for items in range(len(self.thelist)):
-     assert type(self.thelist[items])==list
-     assert len(self.thelist[0]) == len(self.thelist[items])
-     for things in range(len(self.thelist[items])):
-       assert type(self.thelist[items][things])==int
+   def __init__(self,thelist: list):
+       self.thelist=thelist
+       for items in range(len(self.thelist)):
+           assert type(self.thelist[items]) == list
+           assert len(self.thelist[0]) == len(self.thelist[items])
+           for things in range(len(self.thelist[items])):
+               assert type(self.thelist[items][things]) == int
 
- def __str__(self):
-   return str(self.thelist)
+   def __str__(self):
+       return str(self.thelist)
 """
 
 
