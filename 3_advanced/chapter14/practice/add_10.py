fb584a77c5e14633bc5fe82fc3b34eb3edf4bd31
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-09 22:12:48 -0500

    Update add_10.py

--- 3_advanced/chapter14/practice/add_10.py
+++ 3_advanced/chapter14/practice/add_10.py
@@ -1,7 +1,7 @@
 # Problem name: add_10
-# A messy teacher named Bob would like to add 10 points to each 
+# A messy teacher named Bob would like to add 10 points to each
 # student’s recent test score.
-# There are four students, and going from highest score to lowest 
+# There are four students, and going from highest score to lowest
 # score, it is Mike, Dan, Stan, and Ban.
 # Add 10 to each score and assign those values to the correct student.
 # Solve this problem by adding no more than 2 lines of code.
