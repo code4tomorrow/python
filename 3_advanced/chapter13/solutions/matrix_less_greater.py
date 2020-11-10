4a70eac2eb3eac05ea5c58a552c37046348a3c86
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-09 22:40:48 -0500

    Update matrix_less_greater.py

--- 3_advanced/chapter13/solutions/matrix_less_greater.py
+++ 3_advanced/chapter13/solutions/matrix_less_greater.py
@@ -14,16 +14,16 @@ import math
 This is the unmodified Matrix class code.
 
 class Matrix:
- def __init__(self,thelist: list):
-   self.thelist=thelist
-   for items in range(len(self.thelist)):
-     assert type(self.thelist[items])==list
-     assert len(self.thelist[0]) == len(self.thelist[items])
-     for things in range(len(self.thelist[items])):
-       assert type(self.thelist[items][things])==int
+   def __init__(self,thelist: list):
+       self.thelist=thelist
+       for items in range(len(self.thelist)):
+           assert type(self.thelist[items]) == list
+           assert len(self.thelist[0]) == len(self.thelist[items])
+           for things in range(len(self.thelist[items])):
+               assert type(self.thelist[items][things]) == int
 
- def __str__(self):
-   return str(self.thelist)
+   def __str__(self):
+       return str(self.thelist)
 """
 
 
@@ -36,6 +36,7 @@ class Matrix:
             assert len(self.thelist[0]) == len(self.thelist[items])
             for things in range(len(self.thelist[items])):
                 assert type(self.thelist[items][things]) == int
+        self.froebiannorm()
 
     def froebiannorm(self):
         self.squared = 0
