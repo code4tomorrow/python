d83f2429969e6fb49aa16d367c7ccc32325e69c3
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-09 22:40:56 -0500

    Update matrix_frobenius_norm.py

--- 3_advanced/chapter13/solutions/matrix_frobenius_norm.py
+++ 3_advanced/chapter13/solutions/matrix_frobenius_norm.py
@@ -15,16 +15,16 @@ import math
 This is the unmodified Matrix class code.
 
 class Matrix:
- def __init__(self,thelist: list):
-   self.thelist=thelist
-   for items in range(len(self.thelist)):
-     assert type(self.thelist[items])==list
-     assert len(self.thelist[0]) == len(self.thelist[items])
-     for things in range(len(self.thelist[items])):
-       assert type(self.thelist[items][things])==int
+   def __init__(self,thelist: list):
+       self.thelist=thelist
+       for items in range(len(self.thelist)):
+           assert type(self.thelist[items]) == list
+           assert len(self.thelist[0]) == len(self.thelist[items])
+           for things in range(len(self.thelist[items])):
+               assert type(self.thelist[items][things]) == int
 
- def __str__(self):
-   return str(self.thelist)
+   def __str__(self):
+       return str(self.thelist)
 """
 
 
