2ddd0b3bd923f5b60475879ef5a0f1657be2547d
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-11-09 18:47:43 -0800

    Update car.py

--- 3_advanced/chapter13/solutions/car.py
+++ 3_advanced/chapter13/solutions/car.py
@@ -6,9 +6,9 @@ least the attributes “year, original price (aka og price), and
 mileage.” Also, follow these guidelines.
 
 --When using str() on a car, it should return the year, original
-  price, mileage, and adjusted price. 
+  price, mileage, and adjusted price.
 --When adding, it should add the value to its mileage before
-  adjusting the adjusted price. 
+  adjusting the adjusted price.
 --When multiplying, it should multiply the mileage by the value
   before adjusting the adjusted price
 --(While subtracting or dividing mileage on a car to sell it is
