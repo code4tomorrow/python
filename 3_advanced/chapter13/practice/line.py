ebee75289f1dfc33290e878b53263de087ec81d2
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-14 17:44:11 -0500

    Update line.py

--- 3_advanced/chapter13/practice/line.py
+++ 3_advanced/chapter13/practice/line.py
@@ -2,7 +2,7 @@
 Write a class called Line which will take the arguments slope
 and intercept in its constructor. When we print the class,
 the __str__ method should return a string with the line expressed
-in the form  “y=mx+b” where m and b are the slope and intercept
+in the form "y=mx+b" where m and b are the slope and intercept
 respectively.
 """
 
