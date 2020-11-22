1e2e592c314d811473f49f46e99071bab47fd372
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-22 13:29:09 -0500

    Update bob_selection.py

--- 3_advanced/chapter14/practice/bob_selection.py
+++ 3_advanced/chapter14/practice/bob_selection.py
@@ -1,11 +1,12 @@
 """
 Problem Name: bob_selection
-Bob is choosing a person to go to the moon with him. The way he chooses 
-is quite strange. He will choose the first person from a list given to 
-him whose age is divisible by 5 and whose index within the list is divisible 
-by 5. If he does find such a person, print the person’s name. If he doesn’t, 
-don’t print anything. The list given to him contains lists which contain 
-the person’s name and age. Use enumerate to solve this problem.
+Bob is choosing a person to go to the moon with him. The way he
+chooses is quite strange. He will choose the first person from a
+list given to him whose age is divisible by 5 and whose index
+within the list is divisible by 5. If he does find such a person,
+print the person’s name. If he doesn’t, don’t print anything. The
+list given to him contains lists which contain the person’s name
+and age. Use enumerate to solve this problem.
 """
 
 # the list is given to him
