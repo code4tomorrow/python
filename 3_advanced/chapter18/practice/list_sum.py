feceffc48d908eb1cdc0342eb7c731930a69cd22
Lint Action <lint-action@samuelmeuli.com>
2020-11-25 23:41:03 +0000

    Fix code style issues with Black

--- 3_advanced/chapter18/practice/list_sum.py
+++ 3_advanced/chapter18/practice/list_sum.py
@@ -1,12 +1,12 @@
 def listsum(arr):
-  total = 0
+    total = 0
 
-  for i in arr:
-    if isinstance(i, list):
-      # add here and remove "pass"
-      pass
-    else: 
-      # add here and remove "pass"
-      pass
+    for i in arr:
+        if isinstance(i, list):
+            # add here and remove "pass"
+            pass
+        else:
+            # add here and remove "pass"
+            pass
 
-  return total
+    return total
