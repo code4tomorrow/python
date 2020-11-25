1e2a64276d598ade42d2f1360dcf7b012377f918
Lint Action <lint-action@samuelmeuli.com>
2020-11-25 22:58:16 +0000

    Fix code style issues with Black

--- 3_advanced/chapter18/solutions/koch_curve.py
+++ 3_advanced/chapter18/solutions/koch_curve.py
@@ -1,15 +1,19 @@
 import turtle
+
 bob = turtle.Turtle()
 
+
 def kochcurve(x):
-  if x<3: bob.fd(x)
-  else:
-    kochcurve(x/3)
-    bob.left(60)
-    kochcurve(x/3)
-    bob.right(120)
-    kochcurve(x/3)
-    bob.left(60)
-    kochcurve(x/3)
+    if x < 3:
+        bob.fd(x)
+    else:
+        kochcurve(x / 3)
+        bob.left(60)
+        kochcurve(x / 3)
+        bob.right(120)
+        kochcurve(x / 3)
+        bob.left(60)
+        kochcurve(x / 3)
+
 
 kochcurve(90)  # doesn't to be 90, could be any number
