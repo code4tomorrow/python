0fa2de594a2f7e5a7a4689e0090fd70de18be670
Lint Action <lint-action@samuelmeuli.com>
2020-11-25 23:34:04 +0000

    Fix code style issues with Black

--- 3_advanced/chapter18/practice/koch_curve.py
+++ 3_advanced/chapter18/practice/koch_curve.py
@@ -4,9 +4,9 @@ bob = turtle.Turtle()
 
 
 def kochcurve(x):
-  if x<3: 
-    # add here and remove "pass"
-    pass
-  else:
-    # add here and remove "pass"
-    pass
+    if x < 3:
+        # add here and remove "pass"
+        pass
+    else:
+        # add here and remove "pass"
+        pass
