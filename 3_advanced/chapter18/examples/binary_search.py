e4a629b15b16cad81981d0f44bd5a9671c3728bd
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-11-25 15:15:29 -0800

    Update binary_search.py

--- 3_advanced/chapter18/examples/binary_search.py
+++ 3_advanced/chapter18/examples/binary_search.py
@@ -1,11 +1,12 @@
 def binary_search(arr, low, high, x):
-    while high >= low:
-        mid = (high + low) / 2
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] < x:
-            return binary_search(arr, mid + 1, high, x)
-        else:
-            return binary_search(arr, low, mid - 1, x)
+  while high >= low:
+    mid = (high + low) / 2
+    if arr[mid] == x:
+      return mid
+    elif arr[mid] < x:
+      return binary_search(arr, mid + 1, high, x)
     else:
-        return -1
+      return binary_search(arr, low, mid - 1, x)
+  else:
+    return -1
+
