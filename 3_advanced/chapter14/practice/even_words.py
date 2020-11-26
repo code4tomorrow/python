211548b8dd4b832001a0760830f8aee5f740414a
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-25 23:47:15 -0500

    Update even_words.py

--- 3_advanced/chapter14/practice/even_words.py
+++ 3_advanced/chapter14/practice/even_words.py
@@ -2,10 +2,10 @@
 # Given a string that the user inputs create a list that contains the
 # square of the lengths of words with an even amount of characters.
 #
-# For example, if the string is “I am cool”, the list would be [4, 16].
+# For example, if the string is "I am cool", the list would be [4, 16].
 # Use a list comprehension to solve it.
 
-# the user inputs the string
+# The user inputs the string
 string = input()
 
 # write your code below
