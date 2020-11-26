f165332c6ee4afcc87f2380be4c9f4b5a056be51
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-25 23:41:21 -0500

    Update square_root_list.py

--- 3_advanced/chapter14/practice/square_root_list.py
+++ 3_advanced/chapter14/practice/square_root_list.py
@@ -2,7 +2,7 @@
 Problem Name: square_root_list
 Take a user given list of numbers and make a list of all the
 square roots of the numbers using list comprehension. Use a
-list comprehension to solve it. 
+list comprehension to solve it.
 Hint: The square root of a number is the same as taking the one half
 power of a number.
 """
