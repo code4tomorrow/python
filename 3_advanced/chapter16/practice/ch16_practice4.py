ab6f557626d36a1422cbcd2193e7a0f8bf03fdc2
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-11-30 13:54:09 -0500

    Add files via upload

---
+++ 3_advanced/chapter16/practice/ch16_practice4.py
@@ -0,0 +1,28 @@
+"""
+The following code is not meant to be run because
+there's no input. Instead, analyze it's running time
+in terms of Big-O. The first two lines are already
+analyzed for you. Do the same for all the other lines.
+The input of the problem is ex_list, and assume it
+has n elements. At the end, put the total running
+time of code. Note: You will be surprised!
+
+
+ex_list = [?,?,?,...]#Input,O(1)
+for i in range(len(ex_list)):#O(n)
+    print(i)
+    ex_list.append(i)
+for i in ex_list:
+    print(i)
+    ex_list.append(i)	
+#Total running time = There is no upper bound, so
+#no Big-O.
+#
+#Explanation: The second for loop will keep looping
+#since ex_list will keep increasing in size each time
+#you loop. You may ask why doesn't the first for loop do
+#the same? That is because the number of times the first
+#for loop loops is set at the very start of the loop,
+#whereas for the second for loop will keep looping until
+#every element is checked.
+"""
