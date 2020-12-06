55f4491c4830584d42d35c35410baaef7401a56b
Lint Action <lint-action@samuelmeuli.com>
2020-12-06 00:16:56 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/solutions/coutable_set.py
+++ 3_advanced/chapter17/solutions/coutable_set.py
@@ -1,35 +1,39 @@
 class CountableSet:
-  def  __init__(self, objs): # objs is the initial list of objects to be inserted
-    self.elems = {}
-    for obj in objs:
-      self.insert(obj)
+    def __init__(
+        self, objs
+    ):  # objs is the initial list of objects to be inserted
+        self.elems = {}
+        for obj in objs:
+            self.insert(obj)
 
-  # insert x into the set one time, increment the count by one
-  # returns True if map didn't already contain the key
-  # x is the integer to be inserted
-  def insert(self, x):
-    if x in self.elems.keys():
-      self.elems[x] += 1
-      return False
-    else:
-      self.elems[x] = 1
-      return True
+    # insert x into the set one time, increment the count by one
+    # returns True if map didn't already contain the key
+    # x is the integer to be inserted
+    def insert(self, x):
+        if x in self.elems.keys():
+            self.elems[x] += 1
+            return False
+        else:
+            self.elems[x] = 1
+            return True
 
-  # decrement the count by one, returns True if map contains key
-  # x is integer to be deleted
-  def delete(self, x):
-    if x in self.elems.keys():
-      self.elems[x] -= 1
-      if self.elems[x] == 0:
-        self.elems.pop(x)
-      return True
-    return False
+    # decrement the count by one, returns True if map contains key
+    # x is integer to be deleted
+    def delete(self, x):
+        if x in self.elems.keys():
+            self.elems[x] -= 1
+            if self.elems[x] == 0:
+                self.elems.pop(x)
+            return True
+        return False
 
-  # returns the number of times x has been inserted into the set
-  # x is the integer being queried
-  def get(self, x):
-    if x in self.elems.keys(): return self.elems[x]
-    return 0
+    # returns the number of times x has been inserted into the set
+    # x is the integer being queried
+    def get(self, x):
+        if x in self.elems.keys():
+            return self.elems[x]
+        return 0
+
 
 x = CountableSet([1, 2, 3, 1])
 x.insert(3)
