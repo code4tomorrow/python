cde8898a01ec77ff9367ed4f88d9a796dfada94a
Lint Action <lint-action@samuelmeuli.com>
2020-12-06 00:13:35 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/solutions/compute_similarity.py
+++ 3_advanced/chapter17/solutions/compute_similarity.py
@@ -1,9 +1,10 @@
 def computeSimilarity(set1, set2):
-  intersectionSize = 0
-  for elem in set1:
-    if elem in set2:
-      intersectionSize += 1
-  unionSize = len(set1) + len(set2) - intersectionSize
-  return intersectionSize / float(unionSize)
+    intersectionSize = 0
+    for elem in set1:
+        if elem in set2:
+            intersectionSize += 1
+    unionSize = len(set1) + len(set2) - intersectionSize
+    return intersectionSize / float(unionSize)
+
 
 print(computeSimilarity({1, 2}, {1, 3}))
