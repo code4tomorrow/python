d6d8085a7675820f52fd546703b8b2a0d14296b6
Lint Action <lint-action@samuelmeuli.com>
2020-12-06 01:31:27 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/solutions/duplicate_detector.py
+++ 3_advanced/chapter17/solutions/duplicate_detector.py
@@ -1,8 +1,9 @@
 def dup_detector(item: list):
-  theset = set(item)
-  if len(theset) < len(item):
-    return True
-  else:
-    return False
+    theset = set(item)
+    if len(theset) < len(item):
+        return True
+    else:
+        return False
 
-print(dup_detector([5,4,3,2,2])) # should print True
+
+print(dup_detector([5, 4, 3, 2, 2]))  # should print True
