190a4609d81ffa9a18cd6b697fad329f66c6fc00
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-05 17:47:53 -0800

    Create tuple_bear.py

---
+++ 3_advanced/chapter17/solutions/tuple_bear.py
@@ -0,0 +1,17 @@
+def tuple_bear(item: list):
+  ourmin = [item[0][0], item[0][1]]
+  for i in range(len(item)):
+    if ourmin[0] > item[i][0]:
+      ourmin[0] = item[i][0]
+      ourmin[1] = item[i][1]
+      i=0
+  return ourmin[1]
+
+tuplelist = [
+  (5,"Freddy"),
+  (3,"Runaway"),
+  (7,"Killer"),
+  (2,"Luscious")
+]
+
+print(tuple_bear(tuplelist))
