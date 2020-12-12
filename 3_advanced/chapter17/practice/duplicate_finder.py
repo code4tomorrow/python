838cee32566dc219c13f1d907fe1789684bb8299
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-12 08:57:01 -0800

    Create duplicate_finder.py

---
+++ 3_advanced/chapter17/practice/duplicate_finder.py
@@ -0,0 +1,7 @@
+# Create a program that finds whether a list contains duplicates
+# should return True or False
+# use sets in your code
+
+def duplicate_finder(lst):
+  # put your code here and remove "pass"
+  pass
