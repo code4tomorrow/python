87caddc39f748a3b0a2b9aa926152c09f034ce7f
Lint Action <lint-action@samuelmeuli.com>
2020-12-12 21:28:34 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/practice/countable_set.py
+++ 3_advanced/chapter17/practice/countable_set.py
@@ -1,24 +1,27 @@
-# Create a class called CountableSet which stores the number of times each element 
-# has been inserted into the CountableSet object. 
+# Create a class called CountableSet which stores the number of times each element
+# has been inserted into the CountableSet object.
 # (Basically, store it like element: #of times inserted into Countable Set)
 # Implement the following class:
 
+
 class CountableSet:
-  def  __init__(self, objs): # objs is the initial list of objects to be inserted
-    pass
+    def __init__(
+        self, objs
+    ):  # objs is the initial list of objects to be inserted
+        pass
 
-  # insert x into the set one time, increment the count by one
-  # returns True if map didn't already contain the key
-  # x is the integer to be inserted
-  def  insert(self, x):
-    pass
+    # insert x into the set one time, increment the count by one
+    # returns True if map didn't already contain the key
+    # x is the integer to be inserted
+    def insert(self, x):
+        pass
 
-  # decrement the count by one, returns True if map contains key
-  # x is integer to be deleted
-  def delete(self, x): 
-    pass
+    # decrement the count by one, returns True if map contains key
+    # x is integer to be deleted
+    def delete(self, x):
+        pass
 
-  # returns the number of times x has been inserted into the set
-  # x is the integer being queried
-  def get(self, x): 
-    pass
+    # returns the number of times x has been inserted into the set
+    # x is the integer being queried
+    def get(self, x):
+        pass
