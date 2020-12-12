80d168f93d1232b5fce4af23ca3ab1d69eeb4514
Lint Action <lint-action@samuelmeuli.com>
2020-12-12 21:58:21 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/practice/challenge (hard math + python)/powerset_generator.py
+++ 3_advanced/chapter17/practice/challenge (hard math + python)/powerset_generator.py
@@ -2,7 +2,7 @@
 all possible subsets that can be formed using the elements in the set. This also includes the empty set
 and the entire set itself. The set of all such subsets is referred to as the powerset."""
 
-def powerset_generator(elements):
-  # put your code here; remove "pass"
-  pass
 
+def powerset_generator(elements):
+    # put your code here; remove "pass"
+    pass
