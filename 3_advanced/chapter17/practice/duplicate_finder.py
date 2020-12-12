6987ff18af914284839f31147ea82cae1f05ddb7
Lint Action <lint-action@samuelmeuli.com>
2020-12-12 16:57:28 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/practice/duplicate_finder.py
+++ 3_advanced/chapter17/practice/duplicate_finder.py
@@ -2,6 +2,7 @@
 # should return True or False
 # use sets in your code
 
+
 def duplicate_finder(lst):
-  # put your code here and remove "pass"
-  pass
+    # put your code here and remove "pass"
+    pass
