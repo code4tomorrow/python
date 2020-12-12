5ce40ff0c168c2158e753b121f0edd2548e71af7
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-12 15:41:21 -0800

    Create sets.py

---
+++ 3_advanced/chapter17/examples/sets.py
@@ -0,0 +1,46 @@
+# initialize a set with the following syntax
+myset = {'item 1', 'item 2', 'any type besides list and dict', 5, 8, 3}
+#or
+myothset = set() # creates an empty set
+notaset = {} # doesn't create a set; creates a dict; remember that
+
+# sets don't always print the same way
+# try it
+print(myset) 
+print(myset)
+print(myset)
+# for this reason, indexing won't work with sets
+# try it
+anotherset = {'this','might','not','be','in','order'}
+for i in anotherset:
+  print(i)
+
+# subsets and supersets
+# a superset is a set that has all the items that a subset has
+oursuperset = {1,3,5,7,9}
+oursubset = {1,5,3}
+
+print(oursuperset.issuperset(oursubset)) # should print True since oursuperset has 1,5, and 3
+print(oursubset.issubset(oursuperset)) # should print True since oursuperset has 1,5, and 3
+print(oursubset.issuperset(oursuperset)) # should print False since oursubset doesn't have 7 or 9
+
+#set methods
+# includes .intersection , .union , .difference , .symettricdifference
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+
+print(set1.intersection(set2)) # will print 3,4,5 which is the common items (the 'intersection')
+print(set1.difference(set2)) # will print 1,2 which is the different items in set 1 (the 'difference')
+print(set2.difference(set1)) # will print 6,7 which is the difference items in set 2 (the 'difference')
+print(set1.symettricdifference(set2)) #will print 1,2,6,7 since those are the different items in both
+print(set1.union(set2)) # will print 1,2,3,4,5,6,7 since those are the unique items
+
+#set methods (cont.)
+# also includes .add , .discard , .remove (.discard and .remove are similar), .pop , .update
+a_set = set()
+a_set.add(2) #can only add 1 element
+a_set.update([8,9,7]) # is a union between a_set and {8,9,7}
+a_set.remove(8) # removes 8; if 8 isn't there, raises a key error
+a_set.discard(9) # removes 9; if 9 isn't there, don't raise an error (do nothing)
+a_set.pop() # removes the first element; in this case 2
+
