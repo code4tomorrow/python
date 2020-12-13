9d1a7cd003d16735fb4337914adc34cdc2ebe075
Lint Action <lint-action@samuelmeuli.com>
2020-12-13 00:06:33 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/examples/tuples.py
+++ 3_advanced/chapter17/examples/tuples.py
@@ -1,7 +1,13 @@
 # initializing a tuple
 mytuple = ()  # is an empty tuple
 myothtuple = (1,)  # tuples with just 1 item need a comma at the end
-mylasttuple = (4, 6, 3, {5, 6}, [7])  # it's a valid tuple; tuples accept all types
+mylasttuple = (
+    4,
+    6,
+    3,
+    {5, 6},
+    [7],
+)  # it's a valid tuple; tuples accept all types
 
 # modifying a tuple
 # you can't modify a tuple's main elements
@@ -15,5 +21,7 @@ anothtuple[3][0] = 6
 # tuple methods
 # includes .index and .count
 lasttupexample = (4, 6, 8, 10, 4, 2)
-print(lasttupexample.count(4)) # prints 2 since there are two 4's
-print(lasttupexample.index(4)) # works just like indexes in lists; prints 0, the first occurence
+print(lasttupexample.count(4))  # prints 2 since there are two 4's
+print(
+    lasttupexample.index(4)
+)  # works just like indexes in lists; prints 0, the first occurence
