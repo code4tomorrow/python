25aa1c51cad1ab2f4b1551346d891a3eb2735f9a
Lint Action <lint-action@samuelmeuli.com>
2020-12-18 03:47:37 +0000

    Fix code style issues with Black

--- 3_advanced/chapter17/practice/challenge (hard math + python)/linear_independence_checker.py
+++ 3_advanced/chapter17/practice/challenge (hard math + python)/linear_independence_checker.py
@@ -1,5 +1,5 @@
-# In linear algebra, a vector (list) of  n vectors (lists) each 
-# containing n integers are considered linearly independent if we 
+# In linear algebra, a vector (list) of  n vectors (lists) each
+# containing n integers are considered linearly independent if we
 # solve the following equation
 # a * x   +  a * x   +  a * x  +  a * x  = 0
 #  1   1      2   2      3   3     n   n
@@ -8,22 +8,22 @@
 # of equations (this is called the trivial solution). Write a method
 # which takes a vector of n vectors each of size n and determine if
 # they are linearly independent. Return true if linearly independent,
-# and false otherwise. 
+# and false otherwise.
 # (for this question, assume n is less than equal to 4 and greater than 1)
 
-# Note: Another way we can determine if n vectors is linearly independent is if 
-# you find that the matrix formed by concatenating the vectors has a nonzero 
-# determinant. This might be somewhat easier 
+# Note: Another way we can determine if n vectors is linearly independent is if
+# you find that the matrix formed by concatenating the vectors has a nonzero
+# determinant. This might be somewhat easier
 # (search up calculating “determinant using cofactor expansion”).
 
 
 def calculatedeterminant(matrix):
-  # put your code here; remove "pass"
-  pass
+    # put your code here; remove "pass"
+    pass
 
 
 def linearindependence(mat):
-  return calculatedeterminant != 0
+    return calculatedeterminant != 0
 
 
 # mat is an example of an acceptable list of 3 (n) lists each containing 3 (n) integers
