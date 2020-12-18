b185200ff760b706bbeb3212647bc61239b7a8a6
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-17 19:38:49 -0800

    Update linear_independence_checker.py

--- 3_advanced/chapter17/practice/challenge (hard math + python)/linear_independence_checker.py
+++ 3_advanced/chapter17/practice/challenge (hard math + python)/linear_independence_checker.py
@@ -1,12 +1,14 @@
-# In linear algebra, a vector (list) of  n vectors (lists) each containing n
-# integers are considered linearly independent if we solve the following equation
+# In linear algebra, a vector (list) of  n vectors (lists) each 
+# containing n integers are considered linearly independent if we 
+# solve the following equation
 # a * x   +  a * x   +  a * x  +  a * x  = 0
 #  1   1      2   2      3   3     n   n
-# for  where they can be any real constant, and we find that all of them equal to
-# 0 being the only possible solution for this system of equations (this is called 
-# the trivial solution). Write a method which takes a vector of n vectors each of
-# size n and determine if they are linearly independent. Return true if linearly 
-# independent, and false otherwise. 
+# for where they can be any real constant, and we find that all of
+# them equal to 0 being the only possible solution for this system
+# of equations (this is called the trivial solution). Write a method
+# which takes a vector of n vectors each of size n and determine if
+# they are linearly independent. Return true if linearly independent,
+# and false otherwise. 
 # (for this question, assume n is less than equal to 4 and greater than 1)
 
 # Note: Another way we can determine if n vectors is linearly independent is if 
@@ -26,4 +28,4 @@ def linearindependence(mat)
 
 # mat is an example of an acceptable list of 3 (n) lists each containing 3 (n) integers
 # hint: if you use mat, it should give you True
-mat = [[1,1,4], [0,0,5], [0,7,8]]
+mat = [[1, 1, 4], [0, 0, 5], [0, 7, 8]]
