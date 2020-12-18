405a78c651871993c7b6975fea644663aace6d51
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-12-18 00:02:25 -0500

    Update compute_similarity.py

--- 3_advanced/chapter17/solutions/compute_similarity.py
+++ 3_advanced/chapter17/solutions/compute_similarity.py
@@ -1,3 +1,11 @@
+# Given two sets of integers A and B (each element in these sets are
+# between 1 and 1000 inclusive), find the similarity of the two sets
+# (the sets are guaranteed to be nonempty). The similarity is a number
+# which is computed by dividing the size of the intersection of the
+# two sets by their union size.
+# Note: the intersection is the # of elements that both sets have in common.
+
+
 def computeSimilarity(set1, set2):
     intersectionSize = 0
     for elem in set1:
