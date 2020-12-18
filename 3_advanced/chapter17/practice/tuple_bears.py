360f526d070f04043f26b5604e06f71aab0f2f8f
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-17 18:54:53 -0800

    Update tuple_bears.py

--- 3_advanced/chapter17/practice/tuple_bears.py
+++ 3_advanced/chapter17/practice/tuple_bears.py
@@ -1,11 +1,14 @@
-# Fred had lost his teddy bear, so his parents are going to the store to buy a replacement for him.
-# You are given a list of length 2(2 elements) tuples, where each tuple represents a teddy bear.
-# Where the first element contains a number showing how similar that that bear is to Fred's original
-# teddy bear(the smaller, the better), and the second element is a string of the teddy bear's name.
-# Find the teddy bear closest to the one Fred lost, and print its name (don’t worry about tuples w/ same #)
+# Fred had lost his teddy bear, so his parents are going to the store
+# to buy a replacement for him. You are given a list of length 2
+# (2 elements) tuples, where each tuple represents a teddy bear. where
+# the first element contains a number showing how similar that that bear is
+# to Fred's original teddy bear(the smaller, the better), and the second
+# element is a string of the teddy bear's name.
+# Find the teddy bear closest to the one Fred lost, and print its name
+# (don’t worry about tuples w/ same #)
 
 
-def tuple_bears(alist: list):  # it doesn't have to be named alist
+def tuple_bears(alist: list):  # it doesn't have to be named a list
     # put your code here and remove pass
     pass
 
