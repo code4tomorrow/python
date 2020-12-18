167bf025279e2d36519fea4030c3de7c97c1adf3
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-17 19:29:56 -0800

    Update powerset_generator.py

--- 3_advanced/chapter17/practice/challenge (hard math + python)/powerset_generator.py
+++ 3_advanced/chapter17/practice/challenge (hard math + python)/powerset_generator.py
@@ -1,6 +1,7 @@
-# Given a set of distinct integers (between 1 and 15 inclusive) (ex: {3,6,2,1}),
-# return an array of all possible subsets that can be formed using the elements
-# in the set. This also includes the empty set and the entire set itself.
+# Given a set of distinct integers (between 1 and 15 inclusive)
+# (ex: {3,6,2,1}), return an array of all possible subsets that can be
+# formed using the elements in the set. This also includes the empty
+# set and the entire set itself.
 # The set of all such subsets is referred to as the powerset.
 
 
