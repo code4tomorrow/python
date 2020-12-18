c1699df57c8fd1e364eff0a6ac7c59dfbe1780de
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-17 19:43:13 -0800

    Update disjoint_set_union.py

--- 3_advanced/chapter17/practice/challenge (hard math + python)/disjoint_set_union.py
+++ 3_advanced/chapter17/practice/challenge (hard math + python)/disjoint_set_union.py
@@ -1,20 +1,20 @@
-"""Given n number of cities (essentially a list of cities numbered from 0 to n-1), and a 
-list of “bridges” with some arbitrary cost (how expensives that bridge is to build)
-which can connect two cities (you can move both directions along it), finda set of bridges
-such that when built you can reach any city from any starting city and that the combined cost
-of building the bridges is the cheapest possible. (this problem is called the minimum spanning tree)
-It should return the minimum total cost of building such a set of bridges (don’t return which those bridges are)
-"""
+# Given n number of cities (essentially a list of cities numbered
+# from 0 to n-1), and a list of “bridges” with some arbitrary cost
+# (how expensives that bridge is to build) which can connect two
+# cities (you can move both directions along it), finda set of bridges
+# such that when built you can reach any city from any starting city
+# and that the combined cost of building the bridges is the cheapest
+# possible. (this problem is called the minimum spanning tree) It
+# should return the minimum total cost of building such a set of
+# bridges (don’t return which those bridges are)
+
 
 # numberOfCities: number of cities that needs to be connected
-# possibleBridges: a vector containing a vectors each of size 3 which denotes [cityA, cityB, cost]
+# possibleBridges: a vector containing a vectors each of size 3
+# which denotes [cityA, cityB, cost]
 def findMinimumCost(numberOfCities, possibleBridges):
     # put your code here; remove pass
     pass
 
 
-edges = [
-    [0, 1, 1],
-    [0, 2, 1],
-    [1, 2, 2],
-]
+edges = [[0, 1, 1], [0, 2, 1], [1, 2, 2]]
