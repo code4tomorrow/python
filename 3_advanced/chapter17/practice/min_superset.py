1c1dd6ffcef52aaaf7bfa83cb24a020310b5e646
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2020-12-17 18:56:51 -0800

    Update min_superset.py

--- 3_advanced/chapter17/practice/min_superset.py
+++ 3_advanced/chapter17/practice/min_superset.py
@@ -1,7 +1,9 @@
-# Given 3 sets of distinct integers, return the size of the superset of minimum size 
-# which is the superset of all the given sets. Implement the following method:
-def findMinSupersetLength(sets)
+# Given 3 sets of distinct integers, return the size of the superset
+# of minimum size which is the superset of all the given sets.
+# Implement the following method:
+def findMinSupersetLength(sets):
   # code here; remove "pass"
   pass
+
 
 sets = [{1, 2, 3}, {2, 3, 5}, {1, 3, 6}]
