cbf4307cb66f735b5678716905da5c3db6c156a7
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-12-18 00:19:52 -0500

    Update linear_independence_checker.py

--- 3_advanced/chapter17/solutions/challenge (hard math + python)/linear_independence_checker.py
+++ 3_advanced/chapter17/solutions/challenge (hard math + python)/linear_independence_checker.py
@@ -1,3 +1,22 @@
+# In linear algebra, a vector (list) of  n vectors (lists) each
+# containing n integers are considered linearly independent if we
+# solve the following equation
+# a * x   +  a * x   +  a * x  +  a * x  = 0
+#  1   1      2   2      3   3     n   n
+# for where they can be any real constant, and we find that all of
+# them equal to 0 being the only possible solution for this system
+# of equations (this is called the trivial solution). Write a method
+# which takes a vector of n vectors each of size n and determine if
+# they are linearly independent. Return true if linearly independent,
+# and false otherwise.
+# (for this question, assume n is less than equal to 4 and greater than 1)
+
+# Note: Another way we can determine if n vectors is linearly independent is if
+# you find that the matrix formed by concatenating the vectors has a nonzero
+# determinant. This might be somewhat easier
+# (search up calculating “determinant using cofactor expansion”).
+
+
 def calculateDeterminant(matrix):
     length = len(matrix)
     if length == 2:
