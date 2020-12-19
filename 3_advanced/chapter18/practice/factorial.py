f79887b05c7e0bc7b7a6d6c955f76fb3a158e8d4
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2020-12-19 17:40:02 -0500

    Update factorial.py

--- 3_advanced/chapter18/practice/factorial.py
+++ 3_advanced/chapter18/practice/factorial.py
@@ -4,8 +4,8 @@
 
 def factorial(n):
     if n == 0:
-        # add here and remove pass
+        # add here and remove "pass"
         pass
     else:
-        # add here and remove pass
+        # add here and remove "pass"
         pass
