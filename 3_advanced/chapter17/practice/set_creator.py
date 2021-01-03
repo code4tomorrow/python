7a3eb645e281fde9a082b9a981450223ae103878
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2021-01-02 23:54:18 -0500

    New Practice Problem Templates for Ch 17

---
+++ 3_advanced/chapter17/practice/set_creator.py
@@ -0,0 +1,10 @@
+# Create an empty set and print the type of it. Create a
+# set from a given dictionary(do set(given_dict)) and print it.
+# Note: The set created from the given dictionary contains
+# only the keys of the dictionary.
+
+def set_creator(given_dict):
+    pass
+    # Remove pass and write your code in here
+
+set_creator({1:"Wall Street", 2: "Main Street", "Tower": 3})
