7327230b07a735fe006525738377bbf5d9e5ed0e
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2021-01-02 23:55:58 -0500

    Created Solutions for New Chapter 17 Practice Problems

---
+++ 3_advanced/chapter17/solutions/ice_cream_shop.py
@@ -0,0 +1,17 @@
+"""
+An ice cream shop keeps all its available flavours
+in a set. Every month, it gets a list of all the
+flavours that are no longer available. Return
+a new set containing the available flavours this
+month after all the no-longer-available flavours
+are removed. It is possible to solve this problem
+using either discard or remove.
+"""
+
+def remove_flavours(avail_flavs, no_longer_flavs):
+    pass
+    # Remove pass and write your code in here
+
+avail_flavs = {"Strawberry", "Blueberry", "Vanilla"}
+remove_flavours(avail_flavs, ["Strawberry", "Vanilla"])
+print(avail_flavs)
---
+++ 3_advanced/chapter17/solutions/tuple_creator.py
@@ -0,0 +1,11 @@
+# Create an empty tuple and print the type of it.
+# Create a tuple from any one element and print the type of it.
+# Create a tuple from a given dictionary and print it.
+# Note: The tuple created from a dictionary will only contain
+# the keys of the dictionary
+
+def tuple_creator(given_dict):
+    pass
+    # Remove pass and write your code in here
+
+tuple_creator({1:"Wall Street", 2: "Main Street", "Tower": 3})
