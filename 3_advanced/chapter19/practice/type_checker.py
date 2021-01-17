08b13c9fbcec942d2ab38891bf272617c18cefe0
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-01-17 14:33:06 -0800

    Create type_checker.py

---
+++ 3_advanced/chapter19/practice/type_checker.py
@@ -0,0 +1,10 @@
+# Create a function that takes one argument and multiplies it by 4.
+# If the argument is not the correct type, print a message saying so
+# It should be able to run through the list provided
+
+def type_checker(x):
+  pass # remove this
+
+arg_list = [4, 'hi', 'obviously NAN', 5.6]
+for i in arg_list:
+  type_checker(i)
