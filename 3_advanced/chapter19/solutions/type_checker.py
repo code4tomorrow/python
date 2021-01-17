e10daee722606a2b825b036fa09c96e13059b519
Lint Action <lint-action@samuelmeuli.com>
2021-01-17 22:35:38 +0000

    Fix code style issues with Black

--- 3_advanced/chapter19/solutions/type_checker.py
+++ 3_advanced/chapter19/solutions/type_checker.py
@@ -2,13 +2,14 @@
 # If the argument is not the correct type, print a message saying so
 # It should be able to run through the list provided
 
+
 def type_checker(x):
-  try:
-    print(x*4)
-  except TypeError:
-    print("That's not a valid number")
+    try:
+        print(x * 4)
+    except TypeError:
+        print("That's not a valid number")
 
 
-arg_list = [4, 'hi', 'obviously NAN', 5.6]
+arg_list = [4, "hi", "obviously NAN", 5.6]
 for i in arg_list:
-  type_checker(i)
+    type_checker(i)
