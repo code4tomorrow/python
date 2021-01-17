cd7da1fa7cc888ed23b0ee09839a2dec4421a290
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-01-17 14:35:12 -0800

    Create type_checker.py

---
+++ 3_advanced/chapter19/solutions/type_checker.py
@@ -0,0 +1,14 @@
+# Create a function that takes one argument and prints arg * 4.
+# If the argument is not the correct type, print a message saying so
+# It should be able to run through the list provided
+
+def type_checker(x):
+  try:
+    print(x*4)
+  except TypeError:
+    print("That's not a valid number")
+
+
+arg_list = [4, 'hi', 'obviously NAN', 5.6]
+for i in arg_list:
+  type_checker(i)
