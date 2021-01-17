d70cacb5c5ff7e6b59e6ea517012c415e0339fcf
Lint Action <lint-action@samuelmeuli.com>
2021-01-17 22:40:35 +0000

    Fix code style issues with Black

--- 3_advanced/chapter19/practice/finally_use.py
+++ 3_advanced/chapter19/practice/finally_use.py
@@ -1,14 +1,15 @@
 # Write a function that takes two parameters and tries to divide
 # parameter 1 by parameter 2 to get a result and print that result.
 # However, if something goes wrong, have an except print a message
-# saying “something went wrong” (optional: have specific messages 
-# for different errors). Finally, it should, no matter what, print 
+# saying “something went wrong” (optional: have specific messages
+# for different errors). Finally, it should, no matter what, print
 # “Goodbye World” when it is done.
 
+
 def finally_use(num1, num2):
-  pass  # change this
-  
+    pass  # change this
+
 
 finally_use(4, 0)
-finally_use(5, 'hi')
+finally_use(5, "hi")
 finally_use(8, 4)
