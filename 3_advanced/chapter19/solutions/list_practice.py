adc18c62c1600017a1cb697e5c4126c8c55694dd
Citrus716 <60357364+Citrus716@users.noreply.github.com>
2021-01-17 20:29:55 -0500

    grammar error

--- 3_advanced/chapter19/solutions/list_practice.py
+++ 3_advanced/chapter19/solutions/list_practice.py
@@ -1,12 +1,12 @@
 # Write a function that accesses a global list. It should try to
 # take the user’s input for how many times to repeat its process.
-# It’s process should be: 1. ask the user for a value (any type)
+# Its process should be: 1. ask the user for a value (any type)
 # 2. Append that value to the list. Once that is done, ask the user
 # to press q to quit or to input a number to access that index of the
 # list. There should be a different error message depending on the type
 # of error raised. Whether or not there are errors, when the user is done
 # (or there is an error), it should print the list and ask the user
-#  whether they would like to continue. If this input is ‘y’, call the
+# whether they would like to continue. If this input is ‘y’, call the
 # function again.
 
 globlist = []
