47bbeeb5f4b9754f246ca889b619033542ca8894
Lint Action <lint-action@samuelmeuli.com>
2021-01-18 01:41:39 +0000

    Fix code style issues with Black

--- 3_advanced/chapter19/practice/int_checker.py
+++ 3_advanced/chapter19/practice/int_checker.py
@@ -2,6 +2,7 @@
 # convert it from a string to an integer. If it fails, send the
 # user a message telling them to input a real integer next time.
 
+
 def int_checker():
     pass  # change this out for the real code
 
