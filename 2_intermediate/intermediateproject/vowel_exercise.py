98c55f1593384247a21c8fbb823bdde9da6dff79
Lint Action <lint-action@samuelmeuli.com>
2021-01-23 18:07:12 +0000

    Fix code style issues with Black

--- 2_intermediate/intermediateproject/vowel_exercise.py
+++ 2_intermediate/intermediateproject/vowel_exercise.py
@@ -2,8 +2,8 @@ words = ["Apple", "Orange", "Candles", "Kara", "orange"]
 
 output = []
 VOWEL_LIST = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-for elem in words: # Loops through every element in words
-    if len(elem) % 2 == 0 or elem[0] in VOWEL_LIST: 
+for elem in words:  # Loops through every element in words
+    if len(elem) % 2 == 0 or elem[0] in VOWEL_LIST:
         num_vowels = 0
         for char in elem:
             if char in VOWEL_LIST:
@@ -15,4 +15,4 @@ for elem in words: # Loops through every element in words
             if char not in VOWEL_LIST:
                 num_consonants += 1
         output.append(num_consonants)
-print(output) # Should print [2, 3, 5, 2, 3] in this case
+print(output)  # Should print [2, 3, 5, 2, 3] in this case
