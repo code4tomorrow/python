96c088d585dd44b53a723177dc62130824c7a4c3
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-01-23 20:28:26 -0800

    Update dice.py
    add the instructions

--- 3_advanced/advancedproject/dice.py
+++ 3_advanced/advancedproject/dice.py
@@ -1,3 +1,37 @@
+# Consider a dice game played between 3 friends. The game consists of 5
+# rounds, in which each player takes turns rolling a die numbered from 1-6.
+# The friends are interested in dice and want to record their game using a
+# Python program.
+
+# Player Class
+
+# Create a Player class to represent a friend. The __init__ method should 
+# initialize 2 instance variables current and outcomes. current represents
+# the player’s most recent roll. outcomes is a dictionary which will store
+# a record of the player’s rolls, where the keys are the possible outcomes
+# of each roll and the values are the number of times the player rolled that
+# number. For example, outcomes[3] represents how many times the player rolled
+# a 3.
+
+# The roll method in this class should update current with a random dice outcome.
+# It should also update the instance variable containing the dictionary. 
+
+# Game Class
+
+# Create a Game class that will simulate the entire dice game. The __init__ method
+# should initialize 2 instance variables turn_log and players. turn_log is a list
+# that has 5 elements (which are also lists) which represent each round. players
+# is a list of Player objects representing the 3 friends.
+
+# The round method will simulate a round in which all the players roll the dice
+# once and turn_log is updated.
+
+# The play method will simulate 5 rounds and also print results of the game.
+# Specifically, the method will print the turn log and outcomes of each player.
+
+# Hint: Import the random module.
+
+
 import random
 
 
