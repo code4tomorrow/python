0fbd5e9f1429160cafe6f6b888fba32422675738
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-02-06 15:45:46 -0800

    Create shelve_practice_1.py

---
+++ 3_advanced/chapter20/practice/shelve_practice_1.py
@@ -0,0 +1,13 @@
+# Use mydatabase.db . Using it, first get all the keys and put them into a list.
+# For help on this, see the hint below. Next, sort the list. Finally, print the
+# corresponding values.
+# To do that, do print((shelfname)[key]) where (shelfname) is the name of your shelf
+# and key is the key.
+
+# Hint: to get a dictionary or shelf’s keys, all you have to do is the following:
+"""
+for key in myshelf.keys():
+     keylist.append(key)
+"""
+# Keep in mind that the “myshelf” is just a name for a dictionary or shelf and
+# that the “keylist” is just a list holding the keys.
