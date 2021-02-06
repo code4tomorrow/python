8529719bc13adca1e3c7bc8ec1a5c2410384350a
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-02-06 15:16:45 -0800

    Create modify_random_text.py

---
+++ 3_advanced/chapter20/solutions/modify_random_text.py
@@ -0,0 +1,10 @@
+import random
+
+myfile = open("blank.txt", "w")
+myfile.write(str(random.randint(0,1000)))
+myfile.close()
+
+refile = open("blank.txt", "r")
+thetext = refile.read()
+print(thetext)
+print(int(thetext) * 4)
