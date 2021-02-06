f3541cc2d4b2ec3f07cc842700767c00d7e60e34
Lint Action <lint-action@samuelmeuli.com>
2021-02-06 23:21:56 +0000

    Fix code style issues with Black

--- 3_advanced/chapter20/practice/hidden_message.py
+++ 3_advanced/chapter20/practice/hidden_message.py
@@ -1,6 +1,5 @@
 # Create a program that reads the below text file and writes (appends) 2
 # newlines and then every 7th word followed by a space
 
-# ex: given “hi”, “ho”, “ha”, “hy”, “he”, “hu”, “we”, “everyone” 
+# ex: given “hi”, “ho”, “ha”, “hy”, “he”, “hu”, “we”, “everyone”
 # it would print 2 newlines and then ‘hi everyone’
-
