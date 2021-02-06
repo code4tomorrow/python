dc275ce3645ab09cd7869c7b5f79d4270b42766b
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-02-06 15:48:19 -0800

    Update shelve_practice_2.py

--- 3_advanced/chapter20/solutions/shelve_practice_2.py
+++ 3_advanced/chapter20/solutions/shelve_practice_2.py
@@ -1,3 +1,8 @@
+# Use mydb.db . Using it, first create a total variable. Then, add
+# all of the shelve’s values to the total. Remember to check if
+# the value is an integer before adding it to the total.
+# After all, shelves can store all types.
+
 import shelve
 
 total = 0
