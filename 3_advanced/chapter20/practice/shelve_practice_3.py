168b36f9e0527f2b2f83e922a02211d30c95e0db
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-02-20 12:29:43 -0800

    Create shelve_practice_3.py

---
+++ 3_advanced/chapter20/practice/shelve_practice_3.py
@@ -0,0 +1,12 @@
+# Create a database to store orders. Next, ask the customer for their
+# name and store that as a variable. Next, ask the customer whether
+# they want to view their previous order or make a new order.
+
+# If they want to make a new order, use the shelf to store the order
+# as the value and the customer’s name as the key.
+
+# If they want to view a previous order, check if their name is in
+# the shelf’s keys. If it is, print their previous order.
+# If not, tell them that they haven’t ordered.
+# Remember to close the shelf.
+# Hint: to store their order, you can do: shelf[name] = order
