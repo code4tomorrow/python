4226c01971689fb94e09bd29005ecc66e67f8b39
Rebecca Dang <ph.rdang@gmail.com>
2021-02-24 12:52:51 -0800

    Move chapter 13 to intermediate
    Update README to reflect changes and updated chapter order for Advanced

---
+++ 2_intermediate/chapter13/practice/matrix_frobenius_norm.py
@@ -0,0 +1,26 @@
+"""
+Write a modified version of the Matrix class(that was defined in
+one of the example problems in this section) so that the __str__
+method instead returns a string containing a single number: the
+matrix's Frobenius norm. The formula for the Frobenius norm will
+be the square root of the sum of all the elements squared in the
+matrix. Also, the unmodified Matrix class code will be given.
+"""
+
+"""
+This is the unmodified Matrix class code.
+
+class Matrix:
+ def __init__(self,thelist: list):
+   self.thelist=thelist
+   for items in range(len(self.thelist)):
+     assert type(self.thelist[items])==list
+     assert len(self.thelist[0]) == len(self.thelist[items])
+     for things in range(len(self.thelist[items])):
+       assert type(self.thelist[items][things])==int
+
+ def __str__(self):
+   return str(self.thelist)
+"""
+
+# write your code below
--- 3_advanced/chapter13/practice/line.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a class called Line which will take the arguments slope
-and intercept in its constructor. When we print the class,
-the __str__ method should return a string with the line expressed
-in the form "y=mx+b" where m and b are the slope and intercept
-respectively.
-"""
-
-# write your code below
