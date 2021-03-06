9deef403710346536a09329d230ece7ff1d0e62c
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-03-06 15:49:00 -0800

    Create json_practice_2.py

---
+++ 3_advanced/chapter20/practice/json_practice_2.py
@@ -0,0 +1,4 @@
+# use the file "wildlife.json"
+# load the data in the JSON file
+# add at least one habitat and corresponding animal(s) to the dictionary
+# finally, write the updated dictionary to the json file.
