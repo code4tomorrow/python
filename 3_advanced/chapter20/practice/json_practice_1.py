9f85a19397dee3938730a8b90a1a786ea0bc0dd5
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-03-06 15:32:30 -0800

    Create json_practice_1.py

---
+++ 3_advanced/chapter20/practice/json_practice_1.py
@@ -0,0 +1,4 @@
+# use the "favorite_foods.json"
+# in that json file, there will be a dictionary called "favorite_foods"
+# print all the unique favorite foods, which will be the values.
+# Save all the names into a dictionary. Write that dictionary into the json file 
