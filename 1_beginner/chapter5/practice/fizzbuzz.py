3eb38dea05e470c326f8cf59bcb468eb7f1acbbb
Rebecca Dang <ph.rdang@gmail.com>
2021-03-12 14:59:56 -0800

    Add fizzbuzz problem

---
+++ 1_beginner/chapter5/practice/fizzbuzz.py
@@ -0,0 +1,20 @@
+# Fizz Buzz
+"""
+Fizz Buzz is a game played between 2 people
+where they take turns counting up starting at 1.
+However, if the number is divisible by 3, the person
+should say "fizz" instead of the number.
+If the number is divisible by 5, the person should
+say "buzz" instead of the number.
+If the number is divisible by both 3 and 5,
+the person should say "fizzbuzz" instead of the number.
+For example, this is how the first 5 results would look like:
+1
+2
+fizz
+4
+buzz
+Write a program that outputs the first 100 results.
+"""
+
+# Write your code here
