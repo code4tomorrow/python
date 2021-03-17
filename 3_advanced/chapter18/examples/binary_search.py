c43d1c85b9f536bf1f0b160a2d75610c455bd982
Rebecca Dang <ph.rdang@gmail.com>
2021-03-17 15:27:30 -0700

    Update binary search example
    Change 'while' to 'if' because it makes more intuitive sense
    Change 'x' to 'key' because that is the conventional name for a searched item

--- 3_advanced/chapter18/examples/binary_search.py
+++ 3_advanced/chapter18/examples/binary_search.py
@@ -1,29 +1,29 @@
 # Code for binary search
 
 
-def binary_search(arr, low, high, x):
+def binary_search(arr, low, high, key):
     """
     Parameters:
-    1)arr is the sorted array in which we will be finding the element
-    2)low is the lower bound of the interval in which we will
-    be finding the element index
-    3)high is the upper bound of the interval in which we will
-    be finding the element index
-    4)x is the element we are trying to find the index of
+    1) arr is the sorted array in which we will be finding the element
+    2) low is the lower bound of the interval in which we will
+       be finding the element index
+    3) high is the upper bound of the interval in which we will
+       be finding the element index
+    4) key is the element we are trying to find the index of
 
-    Output: the index of the element x in the array arr.
+    Output: the index of the element key in the array arr.
     If the element x does not exist in array arr, -1 will
     be returned.
     """
 
-    while high >= low:
+    if high >= low:
         mid = (high + low) // 2
-        if arr[mid] == x:  # Base Case 1
+        if arr[mid] == key:  # Base Case 1
             return mid
-        elif arr[mid] < x:  # Recursive Case 1
-            return binary_search(arr, mid + 1, high, x)
-        else:  # Recursive Case 2
-            return binary_search(arr, low, mid - 1, x)
+        elif arr[mid] < key:  # Recursive Case 1
+            return binary_search(arr, mid + 1, high, key)
+        else:  # Recursive Case 2 (arr[mid] > x)
+            return binary_search(arr, low, mid - 1, key)
     else:  # Base Case 2: element not found
         return -1
 
