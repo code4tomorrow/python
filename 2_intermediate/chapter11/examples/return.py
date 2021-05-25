1a4c8ea4d858b68eaf171a7641fcb45e9e04afc5
Lint Action <lint-action@samuelmeuli.com>
2021-05-25 04:58:32 +0000

    Fix code style issues with Black

--- 2_intermediate/chapter11/examples/return.py
+++ 2_intermediate/chapter11/examples/return.py
@@ -1,10 +1,11 @@
 # The return statement "hands back" a value
 
+
 def average(numbers):
     # returns the average of a given list
-    return sum(numbers) / len(numbers) 
+    return sum(numbers) / len(numbers)
 
-  
+
 numbers = [1, 2, 3]
 
 # assigns average of "numbers" to "avg" and prints it
