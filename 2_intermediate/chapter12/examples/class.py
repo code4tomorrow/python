12aef71ffc23e48f0c2fa9dfecd155279294d955
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-24 22:04:37 -0700

    Add example for 12.1 section

---
+++ 2_intermediate/chapter12/examples/class.py
@@ -0,0 +1,11 @@
+class dot_example:  # use 'class' keyword followed by your class' name
+	  fun = True
+	  difficult = False
+    
+our_example = dot_example()  # instantiate the class
+
+# would print True
+print(our_example.fun)  # our_example is the object, fun is the attribute
+
+# would print False
+print(our_example.difficult)  # our_example is the object, difficult is the attribute
