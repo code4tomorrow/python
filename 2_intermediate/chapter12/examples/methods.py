51e443c89cb107f737d609c4239f72a69dbe32d3
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-24 22:11:37 -0700

    Add example for class methods

---
+++ 2_intermediate/chapter12/examples/methods.py
@@ -0,0 +1,12 @@
+class Tesla:
+    def __init__(self, maxSpeed=120, color="red"):
+        self.maxSpeed = maxSpeed
+        self.color = color
+        
+    # a method; acts just like a function, but needs self keyword
+    def drive(self):
+        print("The car is now driving")
+    
+    
+p1 = Tesla(140, "blue")
+p1.drive()  # will print "The car is now driving"
