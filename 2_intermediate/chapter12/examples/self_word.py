5a00a91b8514405033ac1a0cc242aa563bc15bbf
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-24 22:14:46 -0700

    Add example for self keyword

---
+++ 2_intermediate/chapter12/examples/self_word.py
@@ -0,0 +1,15 @@
+class Tesla:
+	  def __init__(self, maxSpeed=120, color="red"):
+		    self.maxSpeed = maxSpeed
+		    self.color = color
+        
+    def change(self, c):
+        self.color = c
+    
+    
+p1 = Tesla(140, "blue")
+
+p1.change("yellow")
+print(p1.color)  # prints "yellow"
+p1.color = "hello"
+print(p1.color)  # prints "hello"
