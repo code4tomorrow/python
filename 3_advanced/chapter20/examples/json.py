6e59ce00da4887e29933e4d5002a2724bd3b6c84
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-24 23:09:27 -0700

    Update json.py

--- 3_advanced/chapter20/examples/json.py
+++ 3_advanced/chapter20/examples/json.py
@@ -2,17 +2,17 @@
 import json
 x = open('filename.json', 'w')
 topdict = {}
- 
+
 chinese = {
- "hello" : "ni hao",
- "bye" : "zai jian",
- "how are you" : "ni hao ma"
+ "hello": "ni hao",
+ "bye": "zai jian",
+ "how are you": "ni hao ma"
 }
 frenchlist = [34, 1, 2, 6]
- 
+
 topdict["chinese"] = chinese
 topdict["frenchlist"] = frenchlist
-json.dump(topdict, x, indent = 4)
+json.dump(topdict, x, indent=4)
 x.close()
 
 
@@ -22,32 +22,33 @@ x = open('./testit.json', 'r')
 y = json.load(x)
 # assuming that testit.json had been written to
 for key in y:
-  print(key, “, “, y[key])
+    print(key, ", ", y[key])
 x.close()
 
 
 """ Editing a pre-existing JSON file """
 import json
 x = open('filename.json', 'r')
-y = json.load(x) # y becomes the equivalent of a "top_dict"
+y = json.load(x)  # y becomes the equivalent of a "top_dict"
 x.close()
-y["some_key"] = "some value" # the value can be all the normal types that dictionaries can hold
+# the value can be all the normal types that dictionaries can hold
+y["some_key"] = "some value"
 x = open("filename.json", "w")
-json.dump(y, x, indent = 4)
+json.dump(y, x, indent=4)
 x.close()
 
 
 """ json.dumps() method """
 import json
-oldDict = {"fname":"john", "lname":"doe", "age":20}
-print("oldDict:", type(oldDict)) # prints data type of oldDict
-newStr = json.dumps(oldDict) # converts oldDict to string format
-print("newStr:", type(newStr)) #prints data type of newStr
+oldDict = {"fname": "john", "lname": "doe", "age": 20}
+print("oldDict:", type(oldDict))  # prints data type of oldDict
+newStr = json.dumps(oldDict)  # converts oldDict to string format
+print("newStr:", type(newStr))  # prints data type of newStr
 
 
 """ json.loads() method """
 import json
-oldStr = '{"fname":"john","lname":"doe","age":20}'
-print("oldStr:", type(oldStr)) # prints data type of oldStr
-newDict = json.loads(oldStr) # converts oldStr to string format
-print("newDict:", type(newDict)) # prints data type of newDict
+oldStr = '{"fname": "john", "lname": "doe", "age": 20}'
+print("oldStr:", type(oldStr))  # prints data type of oldStr
+newDict = json.loads(oldStr)  # converts oldStr to string format
+print("newDict:", type(newDict))  # prints data type of newDict
