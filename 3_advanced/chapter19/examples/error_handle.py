87d153c2e5cf8a612adf891642c76b284e756eb2
Lint Action <lint-action@samuelmeuli.com>
2021-05-25 05:50:47 +0000

    Fix code style issues with Black

--- 3_advanced/chapter19/examples/error_handle.py
+++ 3_advanced/chapter19/examples/error_handle.py
@@ -1,18 +1,18 @@
 try:
-	x = 1
-	y = "hi"
-	x + y
-except TypeError: #this will only run if there's a TypeError
-	print("incorrect types, try again")
-except NameError: #this will only run if there's a NameError
-	print("maybe you forgot to create that")
-else: #this will run if no error occurs
-  print("everything good here!")
+    x = 1
+    y = "hi"
+    x + y
+except TypeError:  # this will only run if there's a TypeError
+    print("incorrect types, try again")
+except NameError:  # this will only run if there's a NameError
+    print("maybe you forgot to create that")
+else:  # this will run if no error occurs
+    print("everything good here!")
 finally:
-  #will run no matter what
-	print("x is", x, "\ny is", y)
-  
-  
+    # will run no matter what
+    print("x is", x, "\ny is", y)
+
+
 string = "goodbye"
 assert string == "hello", "string is not hello"
-print(string) #this will not be run because assert raises an exception
+print(string)  # this will not be run because assert raises an exception
