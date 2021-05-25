135a99c9e3546b7c590ef9a1b40abbc7f45b2d47
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-24 23:15:36 -0700

    Update self_word.py

--- 2_intermediate/chapter12/examples/self_word.py
+++ 2_intermediate/chapter12/examples/self_word.py
@@ -2,11 +2,11 @@ class Tesla:
     def __init__(self, maxSpeed=120, color="red"):
 	self.maxSpeed = maxSpeed
 	self.color = color
-        
+
     def change(self, c):
         self.color = c
-    
-    
+
+
 p1 = Tesla(140, "blue")
 
 p1.change("yellow")
