b1ae7057979c1739940e614fc491f9ef09da2089
Lint Action <lint-action@samuelmeuli.com>
2021-05-25 05:55:25 +0000

    Fix code style issues with Black

--- 3_advanced/chapter20/examples/text_files.py
+++ 3_advanced/chapter20/examples/text_files.py
@@ -1,17 +1,17 @@
 # "w" stands for "write"
-myfile = open("mytext.txt","w")
+myfile = open("mytext.txt", "w")
 
 # "r" stands for "read"
-myfile = open("mytext.txt","r")
+myfile = open("mytext.txt", "r")
 
 # "a" stands for "append"
-myfile = open("mytext.txt","a")
+myfile = open("mytext.txt", "a")
 
 
 # writes into a mytext.txt
 # "hello worldhi again"
-myfile.write('hello world')
-myfile.write('hi again')
+myfile.write("hello world")
+myfile.write("hi again")
 
 
 # takes data from mytext.txt and prints it
