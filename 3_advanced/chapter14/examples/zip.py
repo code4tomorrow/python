684a97457372a0e7bb555d4f937a8c1002f9d3a9
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-24 22:33:11 -0700

    Add examples for zip function

---
+++ 3_advanced/chapter14/examples/zip.py
@@ -0,0 +1,23 @@
+a = [1, 2, 3]
+b = ['a', 'b', 'c']
+c = ['!', '@', '#']
+
+G = zip(a, b, c)
+
+print(list(G))
+# prints [(1, 'a', '!'), (2, 'b', '@'), (3, 'c', '#')]
+
+
+list_one = [1, 2]
+list_two = [41]
+
+for pair in zip(list_one, list_two):
+	print(pair)
+# this would only print (1, 41)
+
+
+countries = [' Japan', 'America', 'South Korea', ' China']
+numbers = [1, 2, 3, 4]
+dict1 = dict(zip(countries, numbers))
+print(dict1)
+# prints {' China': 4, ' Japan': 1, 'America': 2, 'South Korea': 3}
