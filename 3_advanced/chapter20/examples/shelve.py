eebf153c04a335795cb5a5cc00ed3c62f5b1a0f2
Lint Action <lint-action@samuelmeuli.com>
2021-05-25 05:56:49 +0000

    Fix code style issues with Black

--- 3_advanced/chapter20/examples/shelve.py
+++ 3_advanced/chapter20/examples/shelve.py
@@ -2,12 +2,12 @@ import shelve
 
 """this will create a database if you don’t already have one and open it if you do"""
 
-myshelf = shelve.open('mydatabase') 
+myshelf = shelve.open("mydatabase")
 
-myshelf['key1'] = 4 
+myshelf["key1"] = 4
 
-#remember, while the key must be a string, the value can be any type
+# remember, while the key must be a string, the value can be any type
 
-print(myshelf['key1']) #will give me 4
+print(myshelf["key1"])  # will give me 4
 
 myshelf.close()
