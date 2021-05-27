16145d052c740835b72a4277ad7c7d633d8944e8
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-27 16:17:54 -0700

    Add clarification for self keyword

--- 2_intermediate/chapter12/examples/self_word.py
+++ 2_intermediate/chapter12/examples/self_word.py
@@ -1,3 +1,9 @@
+# The self keyword is used when you want a method or
+# attribute to be for a specific object. This means that,
+# down below, each Tesla object can have different maxSpeed
+# and colors from each other.
+
+
 class Tesla:
     def __init__(self, maxSpeed=120, color="red"):
         self.maxSpeed = maxSpeed
@@ -8,8 +14,15 @@ class Tesla:
 
 
 p1 = Tesla(140, "blue")
+p2 = Tesla(100, "blue")
 
-p1.change("yellow")
-print(p1.color)  # prints "yellow"
-p1.color = "hello"
-print(p1.color)  # prints "hello"
+
+# Notice how, when we use the self keyword, each object can
+# have different attributes even though they are from the 
+# same class.
+
+p1.change("green")
+print(p1.color)  # prints "green"
+
+p2.change("yellow")
+print(p2.color)  # prints "yellow"
