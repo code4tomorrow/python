a8f18cb6b13bbf273183a1fdc8772ea04e07659c
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-27 16:25:57 -0700

    Add clarifying comments about inheritance

--- 2_intermediate/chapter12/examples/inheritance.py
+++ 2_intermediate/chapter12/examples/inheritance.py
@@ -1,9 +1,13 @@
+# Inheritance in coding is when one "child" class receives
+# all of the methods and attributes of another "parent" class
+
+
 class Test:
     def __init__(self):
         self.x = 0
 
-
-class Derived_Test(Test):  # makes Derived_Test a child of Test
+# class Derived_Test inherits from class Test
+class Derived_Test(Test):
     def __init__(self):
         Test.__init__(self)  # do Test's __init__ method
         # Test's __init__ gives Derived_Test the attribute 'x'
@@ -11,4 +15,7 @@ class Derived_Test(Test):  # makes Derived_Test a child of Test
 
 
 b = Derived_Test()
+
+# Derived_Test now has an attribute "x", even though
+# it originally didn't
 print(b.x, b.y)
