17bd073fd82c442054100677787e2732873b399d
Ben <71541167+BenVN123@users.noreply.github.com>
2021-05-27 16:13:07 -0700

    Add small clarification for class methods

--- 2_intermediate/chapter12/examples/methods.py
+++ 2_intermediate/chapter12/examples/methods.py
@@ -3,10 +3,10 @@ class Tesla:
         self.maxSpeed = maxSpeed
         self.color = color
 
-    # a method; acts just like a function, but needs self keyword
+    # a method: acts just like a function, but needs the self keyword
     def drive(self):
         print("The car is now driving")
 
 
 p1 = Tesla(140, "blue")
-p1.drive()  # will print "The car is now driving"
+p1.drive()  # will execute the drive method from class Tesla
