50c00b699b28f6fab23bc3cba3a076e06c4741ee
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-05-29 09:22:43 -0700

    Add more description
    Add more indepth on why recursionerror happens, what recursionerror is, and how you can potentially change the maximum recursion limit.

--- 3_advanced/chapter18/examples/infinite_recursion.py
+++ 3_advanced/chapter18/examples/infinite_recursion.py
@@ -8,4 +8,17 @@ def recurse(i):
     recurse(i)
 
 
-recurse(0)
+recurse(0)  # this will result in the following message:
+# RecursionError: maximum recursion depth exceeded while
+# calling a Python object
+
+# RecursionError happens when you exceed your maximum
+# recursion limit. By default, it is set to 1000
+# you can check the maximum recursion depth by doing
+# import sys
+# sys.getrecursionlimit()
+# you can change the maximum recursion depth by doing
+# import sys
+# sys.setrecursionlimit()
+# However, this can be dangerous, so only do it if you
+# know what you're doing.
