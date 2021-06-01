6390d384acbdf84ef0541a9235296d5d09d8cdd6
Ben <71541167+BenVN123@users.noreply.github.com>
2021-06-01 12:27:25 -0700

    Add more methods to example

--- 3_advanced/chapter20/examples/text_files.py
+++ 3_advanced/chapter20/examples/text_files.py
@@ -12,8 +12,9 @@ myfile = open("mytext.txt", "w")
 
 # writes into a mytext.txt
 # "hello worldhi again"
-myfile.write("hello world")
-myfile.write("hi again")
+myfile.write("hello world\n")
+myfile.write("hi again\n")
+myfile.write("helloooo")
 
 
 # saves file
@@ -27,3 +28,42 @@ myfile = open("mytext.txt", "r")
 # takes data from mytext.txt and prints it
 mydata = myfile.read()
 print(mydata)
+
+
+# determines if myfile is readable
+print(myfile.readable())
+
+
+print(myfile.readline())  # prints the first line
+print(myfile.readline())  # prints the second line
+print(myfile.readline())  # prints the third line
+
+
+# determines if you can set your position in myfile
+print(myfile.seekable())
+
+
+# sets your position to the 0th index
+myfile.seek(0)
+
+
+# prints a list of all the lines in the file
+print(myfile.readlines())
+
+
+# prints your position in a file
+print(myfile.tell())
+
+
+# determines if you can write into myfile
+print(myfile.writable())
+
+
+myfile = open("mytext.txt", "w")
+
+
+# writes lines from provided list into myfile
+myfile.writelines(["line one\n", "line 2"])
+
+
+myfile.close()
