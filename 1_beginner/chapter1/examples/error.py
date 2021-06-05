1ddecafa973e089e14267a790e3d71c38373b5f6
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-06-05 16:22:24 -0700

    Add example of errors; Ch.1.4

---
+++ 1_beginner/chapter1/examples/error.py
@@ -0,0 +1,8 @@
+# Syntax Errors
+prnt("Hello") # this would be a syntax error
+# someone misspelled 'print' as 'prnt'
+
+    print("Hello") # this would also result in an error since there's an extra indent
+
+# Runtime Error
+print(1 / 0) # this would result in a ZeroDivision error
