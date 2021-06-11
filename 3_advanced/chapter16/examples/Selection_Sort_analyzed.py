2b80eb1b3bd498a00a5376641849a215f9141e0b
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-06-10 17:50:27 -0700

    Update Selection_Sort_analyzed.py

--- 3_advanced/chapter16/examples/Selection_Sort_analyzed.py
+++ 3_advanced/chapter16/examples/Selection_Sort_analyzed.py
@@ -2,8 +2,8 @@ arr = [?, ?, ?]  # this is the input, so we're not including it in analysis
 for first_idx in range(len(arr)):  # O(n)
     min_idx = first_idx  # O(1) * O(n) = O(n)
     for second_idx in range(first_idx + 1, len(arr)):  # O(n) * O(n) = O(n^2)
-        if arr[second_idx] < arr[min_idx]:  # O(n) * O(n) = O(n^2)
-            min_idx = second_idx  # O(n) * O(n) = O(n^2)
+        if arr[second_idx] < arr[min_idx]:  # O(1) * O(n) * O(n) = O(n^2)
+            min_idx = second_idx  # O(1) * O(n) * O(n) = O(n^2)
     arr[first_idx], arr[min_idx] = arr[min_idx], arr[first_idx]  # O(1) * O(n) = O(n)
 
 # Sum = O(n) + O(n) + O(n^2) + O(n^2) + O(n^2) + O(n)
