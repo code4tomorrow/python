44ccd3cd63813eb075e8eb9044ab9feb5ed45fc8
Lint Action <lint-action@samuelmeuli.com>
2021-06-11 00:58:58 +0000

    Fix code style issues with Black

--- 3_advanced/chapter16/examples/Selection_Sort_analyzed.py
+++ 3_advanced/chapter16/examples/Selection_Sort_analyzed.py
@@ -4,7 +4,10 @@ for first_idx in range(len(arr)):  # O(n)
     for second_idx in range(first_idx + 1, len(arr)):  # O(n) * O(n) = O(n^2)
         if arr[second_idx] < arr[min_idx]:  # O(1) * O(n) * O(n) = O(n^2)
             min_idx = second_idx  # O(1) * O(n) * O(n) = O(n^2)
-    arr[first_idx], arr[min_idx] = arr[min_idx], arr[first_idx]  # O(1)*O(n) = O(n)
+    arr[first_idx], arr[min_idx] = (
+        arr[min_idx],
+        arr[first_idx],
+    )  # O(1)*O(n) = O(n)
 
 # Sum = O(n) + O(n) + O(n^2) + O(n^2) + O(n^2) + O(n)
 # Sum = 3*O(n) + 3*O(n^2)
