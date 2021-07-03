9323751e87b2286763a10b687e94c773578f5964
Ben <71541167+BenVN123@users.noreply.github.com>
2021-07-02 17:40:18 -0700

    Solution to shopping practice problem

---
+++ 2_intermediate/chapter11/solutions/shopping.py
@@ -0,0 +1,25 @@
+"""
+Code a function named shopping that will print the number
+of items that a customer would like to buy. It will take in
+an unknown number of parameters, each representing
+one item. After that, the function will ask the customer,
+"Are you sure you would like to buy" and stating the number
+of items the customer wants to purchase.
+
+
+===Example 1===
+# Parameters: "soap", "brush", "comb"
+# Output: "Are you sure you would like to buy 3 items?"
+
+===Example 2===
+# Parameters: "lotion", "shoes", "pencil", "crayon"
+# Output: "Are you sure you would like to buy 4 items?"
+"""
+
+
+def shopping(*args):
+    item_number = len(args)
+    print("Are you sure you would like to buy", item_number, "items?")
+
+
+shopping("soap", "brush", "comb")
