97c1e2127e548f3ddf9d4098adc69299a4b490d5
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2021-07-21 21:13:31 -0700

    Fix styling errors

--- guessthepassword.py
+++ guessthepassword.py
@@ -1,19 +1,20 @@
-#Directions: The goal of this exercise is to create a game where the user has to guess a 
-certain password that you set and see how many tries it takes for that user to guess correctly
+# Directions: The goal of this exercise is to create
+# a game where the user has to guess a certain password that
+# you set and see how many tries it takes for that user to guess correctly
 
-#start with assigning the password to some variable
+# start with assigning the password to some variable
 pas = str("password")
 
 # set an input so it will appear in the console and ask the user
-guess= input("Enter the password:")
+guess = input("Enter the password:")
 
-#set a counter to count the number of guesses
+# set a counter to count the number of guesses
 counter = 1
-#set a while loop to check if the user guess correctly and count the number of guesses
+
+# set a while loop to check if the user guess correctly and count the number of guesses
 while guess != pas:
-  guess = input("Incorrect Password. Try Again:")
-  counter +=1
+    guess = input("Incorrect Password. Try Again:")
+    counter += 1
+
 # print the results
-
-
-print("Nice Job. Unlocked. It took you " + str(counter)+ " tries")
+print(f"Nice Job. Unlocked. It took you {str(counter)} tries")
