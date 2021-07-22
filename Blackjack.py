bb8a39ac2097c28f7f266d9e3e3e60ec5c18e685
Lint Action <lint-action@samuelmeuli.com>
2021-07-22 03:58:42 +0000

    Fix code style issues with Black

--- Blackjack.py
+++ Blackjack.py
@@ -13,19 +13,19 @@ userList = []
 
 # append two random cards to start the game
 for i in range(2):
-  dealerList.append(random.randint(2,11))
-  userList.append(random.randint(2,11))
+    dealerList.append(random.randint(2, 11))
+    userList.append(random.randint(2, 11))
 # print the first two cards
 print("Here is the dealer's cards:" + str(dealerList))
 print("Here is the user's cards:" + str(userList))
-if sum(userList)==21:
-  print("User Won")
-  exit()
-if sum(dealerList)==21:
-  print("Dealer won")
-  exit()
-  
-#ask hit or stay... write a functionn for hit and stay...conditional for the typed in key
+if sum(userList) == 21:
+    print("User Won")
+    exit()
+if sum(dealerList) == 21:
+    print("Dealer won")
+    exit()
+
+# ask hit or stay... write a functionn for hit and stay...conditional for the typed in key
 # hit means to take another card and stay means to play with already drawn cards
 
 # print the instructions
@@ -33,55 +33,49 @@ ask = input("Type in H to hit and S to stay:")
 
 # write a function for hit to use in multiple scenarios
 def hit(cards):
-  cards.append(random.randint(2,11))
- 
-# while loop will keep checking the two players' cards to see if they reached 21 or not
-while ask == "H" or ask =="h":
-  hit(userList)
-  print("Here is your hand:" + str(userList))
+    cards.append(random.randint(2, 11))
 
-  if sum(userList) >21:
-    print("User is Busted")
-    exit()
-  if sum(userList) ==21:
-    print("User Won")
-    exit()
-  ask = input("Type in H to hit and S to stay:")
+
+# while loop will keep checking the two players' cards to see if they reached 21 or not
+while ask == "H" or ask == "h":
+    hit(userList)
+    print("Here is your hand:" + str(userList))
+
+    if sum(userList) > 21:
+        print("User is Busted")
+        exit()
+    if sum(userList) == 21:
+        print("User Won")
+        exit()
+    ask = input("Type in H to hit and S to stay:")
 
 # if statement for "stay"
-if ask == "S" or ask=="s":
-  print("Here is your hand:" + str(userList))
-
-  
-#when hit- append a anotehr random number into the list of the dealer/userList
-#when stay- just go to next play and print out the list
-#while dealer less than 17 append new cards to the list
-
-while sum(dealerList) <17:
-  hit(dealerList)
-  print("Here is dealer's hand:" + str(dealerList))
-
-#compare cards for win
-if sum(dealerList) >21:
-  print("Dealer is Busted")
-  
-  exit()
-if sum(dealerList)==21 and sum(userList) == 21:
-  
-  print("It is a tie")
-  exit()
-if sum(dealerList) ==21:
-  print("Dealer Won")
-  exit()
-
-if 21- sum(dealerList) > 21-sum(userList):
-  print("User is closer")
-if 21- sum(dealerList) < 21-sum(userList):
-  print("Dealer is closer")
+if ask == "S" or ask == "s":
+    print("Here is your hand:" + str(userList))
 
 
+# when hit- append a anotehr random number into the list of the dealer/userList
+# when stay- just go to next play and print out the list
+# while dealer less than 17 append new cards to the list
 
+while sum(dealerList) < 17:
+    hit(dealerList)
+    print("Here is dealer's hand:" + str(dealerList))
 
+# compare cards for win
+if sum(dealerList) > 21:
+    print("Dealer is Busted")
 
+    exit()
+if sum(dealerList) == 21 and sum(userList) == 21:
 
+    print("It is a tie")
+    exit()
+if sum(dealerList) == 21:
+    print("Dealer Won")
+    exit()
 
+if 21 - sum(dealerList) > 21 - sum(userList):
+    print("User is closer")
+if 21 - sum(dealerList) < 21 - sum(userList):
+    print("Dealer is closer")
