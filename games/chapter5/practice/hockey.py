10a0fd96737e7717236f08e1b87db6cb54a01013
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-07-27 11:39:38 -0400

    Create hockey.py

---
+++ games/chapter5/practice/hockey.py
@@ -0,0 +1,12 @@
+# Make a two-player hockey game! The application will consist
+# of two rectangular paddles, starting on each side of the screen,
+# and one circular ball that players must bounce around. Players can
+# move the paddles in any direction to hit the ball into the goal.
+# If the ball makes contact with safe parts of the screen, it will
+# bounce off at a random angle but in the same general direction
+# (left or right). It will do the same if it makes contact with one
+# of the paddles, but will head towards the opposite general direction
+# instead. If the ball touches the goals on either side of the screen,
+# the application will say “Game Over. Player _ Wins”. You must put
+# your code in classes and have separate keys for each player to
+# move their paddles.
