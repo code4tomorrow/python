023260bebcf60018cf3c285da18f2f98b2a83b3b
Lint Action <lint-action@samuelmeuli.com>
2021-07-27 15:25:48 +0000

    Fix code style issues with Black

--- games/chapter2/practice/bouncing_rect.py
+++ games/chapter2/practice/bouncing_rect.py
@@ -3,13 +3,13 @@
 # For this, please use the given screen and rectangle
 # width and height.
 
-# The rectangle should start in (or close to) the middle of the 
+# The rectangle should start in (or close to) the middle of the
 # screen. It should be moving down and right. If it collides
 # with the screen’s lower or upper boundary, it should reverse
 # its vertical direction. If it collides with the screen’s left
 # or right boundary, it should reverse its horizontal direction.
 
-# Note: you can import the time module as well and use 
+# Note: you can import the time module as well and use
 # time.sleep(0.01)
 # in your mainloop to act as a frame cap to make your rectangle
 # more visible
