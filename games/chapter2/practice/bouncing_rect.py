8f590ebb3e53efb2f461cc93e9f2050846558049
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-07-27 11:09:47 -0400

    Create bouncing_rect.py

---
+++ games/chapter2/practice/bouncing_rect.py
@@ -0,0 +1,12 @@
+# Make a “bouncing rectangle!”
+# For this, please use the given screen and rectangle
+# width and height.
+# The rectangle should start moving up and right. If it collides
+# with the screen’s lower or upper boundary, it should reverse
+# its vertical direction. If it collides with the screen’s left
+# or right boundary, it should reverse its horizontal direction.
+
+SCREEN_SIZE = [600, 400]
+RECT_SIZE = [100, 100]
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
