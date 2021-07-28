a5a64b0b53358593be4e6fa6eb47b962abadaf15
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-07-28 07:37:15 -0400

    Use f"{guesses}"

--- games/chapter1/hangman.py
+++ games/chapter1/hangman.py
@@ -23,7 +23,7 @@ def hangman(endword: str):
     # mainloop
     for i in range(15):
         # take user input
-        guess = input("Guess a letter! You have " + str(guesses) + " guesses left: ")
+        guess = input(f"Guess a letter! You have {guesses} guesses left: ")
 
         # win condition
         if guess == endword:
