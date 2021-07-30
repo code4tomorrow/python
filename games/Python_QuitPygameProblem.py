910dad48efe74159c813283e47ae81c158dabac2
TomM-and-jerry <70043570+TomM-and-jerry@users.noreply.github.com>
2021-07-30 18:08:29 -0400

    Add files via upload

---
+++ games/Python_QuitPygameProblem.py
@@ -0,0 +1,22 @@
+import pygame, sys
+
+pygame.init()
+
+run = True
+width = 500
+height = 500
+white = (255, 255, 255)
+screen = pygame.display.set_mode((width, height))
+screen.fill(white)
+
+#pygame main loop
+while run:
+    pygame.time.delay(50)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            #there are a couple of ways of stopping the pygame loop. One way is to set run = false. Or you can use sys.exit() to stop your program.
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+    pygame.display.update()
