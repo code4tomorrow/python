eabc9110c00e2885759dfb09ccf609c45b7b40e1
Lint Action <lint-action@samuelmeuli.com>
2021-07-30 22:18:45 +0000

    Fix code style issues with Black

--- games/chapter3/practice/Python_MoveRectProblem.py
+++ games/chapter3/practice/Python_MoveRectProblem.py
@@ -9,27 +9,31 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode((width, height))
 screen.fill(white)
 
-#rect[0] = rect color; rect[1] = x-coord; rect[2] = y-coord; rect[3] = width; rect[4] = height
+# rect[0] = rect color; rect[1] = x-coord; rect[2] = y-coord; rect[3] = width; rect[4] = height
 rectangle = [(255, 0, 0), 20, 20, 20, 20]
 
-#pygame main loop
+# pygame main loop
 while run:
     pygame.time.delay(50)
-    #clear the screen by filling it white
+    # clear the screen by filling it white
     screen.fill(white)
-    #draw rect
-    pygame.draw.rect(screen, rectangle[0], pygame.Rect(rectangle[1], rectangle[2], rectangle[3], rectangle[4]))
-    #Check events
+    # draw rect
+    pygame.draw.rect(
+        screen,
+        rectangle[0],
+        pygame.Rect(rectangle[1], rectangle[2], rectangle[3], rectangle[4]),
+    )
+    # Check events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            #there are a couple of ways of stopping the pygame loop. One way is to set run = false. Or you can use sys.exit() to stop your program.
+            # there are a couple of ways of stopping the pygame loop. One way is to set run = false. Or you can use sys.exit() to stop your program.
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
-        #get states of keys
+        # get states of keys
         keysPressed = pygame.key.get_pressed()
-        #recall that y coord decreases as you go up the window; the origin is at the top left corner
+        # recall that y coord decreases as you go up the window; the origin is at the top left corner
         if keysPressed[pygame.K_UP] or keysPressed[pygame.K_w]:
             rectangle[2] -= 5 if rectangle[2] >= 5 else 0
         elif keysPressed[pygame.K_s] or keysPressed[pygame.K_DOWN]:
@@ -37,5 +41,5 @@ while run:
         elif keysPressed[pygame.K_d] or keysPressed[pygame.K_RIGHT]:
             rectangle[1] += 5 if rectangle[1] <= 475 else 0
         elif keysPressed[pygame.K_a] or keysPressed[pygame.K_LEFT]:
-            rectangle[1] -= 5 if rectangle[1] >= 5 else 0        
+            rectangle[1] -= 5 if rectangle[1] >= 5 else 0
     pygame.display.update()
--- games/chapter3/practice/Python_QuitPygameProblem.py
+++ games/chapter3/practice/Python_QuitPygameProblem.py
@@ -9,12 +9,12 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode((width, height))
 screen.fill(white)
 
-#pygame main loop
+# pygame main loop
 while run:
     pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            #there are a couple of ways of stopping the pygame loop. One way is to set run = false. Or you can use sys.exit() to stop your program.
+            # there are a couple of ways of stopping the pygame loop. One way is to set run = false. Or you can use sys.exit() to stop your program.
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
