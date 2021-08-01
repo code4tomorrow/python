23e8b6a71c8516d4b22c18cfed0c5f3f2b612e57
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-08-01 08:58:58 -0400

    provide rectangle list in the template code; edit instructions accordingly

--- games/chapter3/solutions/MoveRectProblem.py
+++ games/chapter3/solutions/MoveRectProblem.py
@@ -1,5 +1,5 @@
 # Build off of your previous code (the code from QuitPygameProblem.py)
-# Draw an object on the screen (a rectangle would suffice).
+# Draw the provided rectangle onto the screen.
 # Move the object up when either the W or up arrow key is pressed;
 # right when either the D or right arrow is pressed; etc.
 
