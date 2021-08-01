757fdbad257c4a6b53506fc8cb96c9dc68e2a01b
chrehall68 <60240707+chrehall68@users.noreply.github.com>
2021-07-31 21:44:15 -0400

    Add instructions to practice problems.

---
+++ games/chapter3/practice/MoveRectProblem.py
@@ -0,0 +1,4 @@
+# Build off of your previous code (the code from QuitPygameProblem.py)
+# Draw an object on the screen (a rectangle would suffice).
+# Move the object up when either the W or up arrow key is pressed;
+# right when either the D or right arrow is pressed; etc.
---
+++ games/chapter3/practice/QuitPygameProblem.py
@@ -0,0 +1,8 @@
+# Create a pygame window.
+# Close the pygame window when either the quit event occurs
+# or the escape key is pressed.
+# Use the provided width and height. Fill the screen with white
+
+width = 500
+height = 500
+white = (255, 255, 255)
