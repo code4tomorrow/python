a91d7f68ec31186d5a264e51cde952d601307d09
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2021-08-09 11:29:16 -0700

    Edit comment

--- 1_beginner/chapter5/solutions/alternating.py
+++ 1_beginner/chapter5/solutions/alternating.py
@@ -1,6 +1,6 @@
 """
 Alternating
-Ask the user for an integer. The print the numbers from 1
+Ask the user for a positive integer. Then print the numbers from 1
 to that number, but alternating in sign. For example, if the input
 was 5, what would be printed is 1, -1, 2, -2, 3, -3, 4, -4, 5.
 (Note, DO NOT include the last negative number).
