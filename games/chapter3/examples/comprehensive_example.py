b0dcd9f9f2ae2ff33af9bef84569f76ecca8f267
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2021-08-21 19:49:02 -0700

    Fix styling

--- games/chapter3/examples/comprehensive_example.py
+++ games/chapter3/examples/comprehensive_example.py
@@ -1,11 +1,11 @@
 import pygame
 pygame.init()
- 
+
 flag = pygame.locals.RESIZABLE
 window = pygame.display.set_mode((500, 400), flag)
- 
+
 pygame.event.set_blocked(pygame.KEYDOWN)
- 
+
 run = True
 while run:
     for event in pygame.event.get():
@@ -18,5 +18,5 @@ while run:
         if event.type == pygame.KEYDOWN:
             # this will never happen because KEYDOWN is blocked
             print(“Down down down!”)
- 
+
 pygame.quit()
