4800964bffa6d4a37d5ee9bc387136b40398419d
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2021-08-31 18:12:59 -0700

    Create blackjack.py

---
+++ games/chapter1/practice/blackjack.py
@@ -0,0 +1,11 @@
+# Directions: The goal of blackjack is to be the first player
+# to get to 21. Each player will draw randomly and the
+# sum of the cards will add to 21.
+
+import random
+
+
+print("Welcome to the game of BlackJack. ")
+print("")
+
+# Add your code here
