0d8cba1bca7f8d401f63ab63f0fa94815ad3ae2f
shiventi <105996940+shiventi@users.noreply.github.com>
2022-05-25 21:09:29 -0700

    Update SpaceCounter.py

--- games/chapter3/solutions/SpaceCounter.py
+++ games/chapter3/solutions/SpaceCounter.py
@@ -1,45 +1,42 @@
-#Create a program that increments a counter every time the space bar is pressed. 
-#This counter should be displayed as text on the pygame window.
-
 import pygame
- 
+
 pygame.init()
 screen = pygame.display.set_mode((400, 400))
- 
-font = pygame.font.SysFont('arial', 70)
+
+font = pygame.font.SysFont("arial", 70)
 
 display_counter = 0
 
 run = True
- 
+
 while run:
 
-	# Render the "display_counter" to the screen 
+    # Render the "display_counter" to the screen
 
-	show_counter = font.render(str(display_counter), True, (255,192,203))
-	
-	# Makes Screen Black
+    show_counter = font.render(str(display_counter), True, (255, 192, 203))
 
-	screen.fill((0, 0, 0))
+    # Makes Screen Black
 
-	# Prints Data on Screen
+    screen.fill((0, 0, 0))
 
-	screen.blit(show_counter, (30,30))
+    # Prints Data on Screen
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+    screen.blit(show_counter, (30, 30))
 
-		# Checks to see if key is pressed
-		if event.type == pygame.KEYDOWN:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-			# Checks to see if the space is pressed
-			if event.key == pygame.K_SPACE:
+        # Checks to see if key is pressed
+        if event.type == pygame.KEYDOWN:
 
-				# Adds one to the counter 
-				display_counter += 1 
+            # Checks to see if the space is pressed
+            if event.key == pygame.K_SPACE:
 
-		# Updates the data
-		pygame.display.update()
-		
+                # Adds one to the counter
+                display_counter += 1
+
+        # Updates the data
+        pygame.display.update()
+
 pygame.quit()
