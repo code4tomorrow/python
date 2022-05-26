dc3f20bf81de01dc0f854d349e470a8c53cedff1
shiventi <105996940+shiventi@users.noreply.github.com>
2022-05-25 21:10:20 -0700

    Update SpaceCounter.py

--- games/chapter3/practice/SpaceCounter.py
+++ games/chapter3/practice/SpaceCounter.py
@@ -1,9 +1,8 @@
-import pygame
-
 #Create a program that increments a counter every time the space bar is pressed. 
 #This counter should be displayed as text on the pygame window.
- 
-while True:
-  
-  #WRITE CODE HERE
 
+import pygame
+
+while True:
+
+  #WRITE CODE HERE
