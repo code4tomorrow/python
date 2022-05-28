4f5fd9603b51cdaa773cf37aaff0d28b65c4e8ee
Harsh Panchal <hpanchal092@gmail.com>
2022-05-27 21:28:29 -0700

    Add Space Counter practice problem

---
+++ games/chapter3/practice/SpaceCounter.py
@@ -0,0 +1,8 @@
+# Build off of your previous code (the code from QuitPygameProblem.py)
+# Draw the provided rectangle onto the screen.
+# Move the object up when either the W or up arrow key is pressed;
+# right when either the D or right arrow is pressed; etc.
+
+import pygame
+
+pygame.init()
