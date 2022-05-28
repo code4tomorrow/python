c4bb6fc6b4c9e4ec3d63a7b4b387fbe24bc7313a
Harsh Panchal <hpanchal092@gmail.com>
2022-05-27 21:38:43 -0700

    Fix problem specifications in practice file

--- games/chapter3/practice/SpaceCounter.py
+++ games/chapter3/practice/SpaceCounter.py
@@ -1,8 +1,7 @@
-# Build off of your previous code (the code from QuitPygameProblem.py)
-# Draw the provided rectangle onto the screen.
-# Move the object up when either the W or up arrow key is pressed;
-# right when either the D or right arrow is pressed; etc.
+# Create a program that increments a counter every time the space bar is
+# pressed. This counter should be displayed as text on the pygame window.
 
 import pygame
 
 pygame.init()
+screen = pygame.display.set_mode((400, 400))
