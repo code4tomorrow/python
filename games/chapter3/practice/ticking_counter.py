47dd3d2045a8427c2b095bd22d3b5dddd54b3cea
Aadya-Jain <106455761+Aadya-Jain@users.noreply.github.com>
2022-06-03 21:54:50 -0400

    Update ticking_counter.py

--- games/chapter3/practice/ticking_counter.py
+++ games/chapter3/practice/ticking_counter.py
@@ -9,7 +9,8 @@
 # to show that they've blown up, and exit the program.
 # Make sure to use some of the methods featured in 3.4!
 
-# imports!
+#import more if needed!
+import pygame
 
 screen = pygame.display.set_mode((400, 400))
 
