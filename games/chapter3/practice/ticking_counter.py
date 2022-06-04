bf8c483f15395cef0baeff8df56f3934adbebad8
Lint Action <lint-action@samuelmeuli.com>
2022-06-04 01:55:11 +0000

    Fix code style issues with Black

--- games/chapter3/practice/ticking_counter.py
+++ games/chapter3/practice/ticking_counter.py
@@ -9,7 +9,7 @@
 # to show that they've blown up, and exit the program.
 # Make sure to use some of the methods featured in 3.4!
 
-#import more if needed!
+# import more if needed!
 import pygame
 
 screen = pygame.display.set_mode((400, 400))
